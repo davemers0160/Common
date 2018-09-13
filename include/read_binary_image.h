f3793b049dfeff9ffb69a2327229a71aedfc31a3
davemes0160 <david.r.emerson@navy.mil>
2018-09-13 13:19:18 -0400

    update to read binary image

--- include/read_binary_image.h
+++ include/read_binary_image.h
@@ -6,22 +6,38 @@
 #include <cstdio>
 
 template <typename T>
-void read_binary_image(std::string filename,uint32_t &height,  uint32_t &width, T* &data)
+void read_binary_image(std::string filename, uint32_t &height,  uint32_t &width, T* &data)
 {
-    FILE* FP = fopen(filename.c_str(), "rb");
-    int32_t result = 0;
+	try
+	{
+		FILE* FP = fopen(filename.c_str(), "rb");
+		int32_t result = 0;
+		
+		height = 0;
+		width = 0;
+		
+		if(!FP) 
+		{
+			std::perror("File opening failed");
+            std::cout << filename << std::endl;
+			return;
+		}
+		
+		// read num of rows then num of cols
+		result = fread(&height, sizeof(uint32_t), 1, FP);
+		result = fread(&width, sizeof(uint32_t), 1, FP);
 
-    // read num of rows then num of cols
-    result = fread(&height, sizeof(uint32_t), 1, FP);
-    result = fread(&width, sizeof(uint32_t), 1, FP);
 
+		data = new T[width*height];
 
-    data = new T[width*height];
+		result = fread(data, sizeof(T), width*height, FP);
 
-    result = fread(data, sizeof(T), width*height, FP);
-
-    fclose(FP);
-
+		fclose(FP);
+	}
+	catch(std::exception e)
+	{
+		std::cout << "Error: " << e.what() << std::endl;
+	}
 }
 
 #endif	// _READ_BINARY_LIDAR_DATA_H
