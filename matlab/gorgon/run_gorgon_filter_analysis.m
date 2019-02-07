fb107ccf84b39267024aa7f6a4fbe1322fa8c716
David Emerson <davemers@iu.edu>
2019-02-06 23:23:11 -0500

    Update run_gorgon_filter_analysis.m

--- matlab/gorgon/run_gorgon_filter_analysis.m
+++ matlab/gorgon/run_gorgon_filter_analysis.m
@@ -24,7 +24,10 @@ listing = listing(3:end);
 commandwindow;
 
 %%
+
+file_id = fopen(fullfile(filepath,'gorgon_filter_analysis.txt'),'w');
 fprintf('%s\n',filepath);
+fprintf(file_id,'%s\n',filepath);
 
 for idx=1:numel(listing)
     if(~listing(idx).isdir)
@@ -33,14 +36,17 @@ for idx=1:numel(listing)
     
     xml_listing = dir(strcat(listing(idx).folder,filesep,listing(idx).name,filesep,'*.xml'));
     fprintf('Running gorgon analysis on %s\n',listing(idx).name);
-    
+    fprintf(file_id,'Running gorgon analysis on %s\n',listing(idx).name);
+   
     for jdx=1:numel(xml_listing)
         [layer, u] = gorgon_filter_analysis(fullfile(xml_listing(jdx).folder,xml_listing(jdx).name));
         fprintf('Layer: %03d\t Unique Classes: %03d\n', layer, numel(u));
+        fprintf(file_id,'Layer: %03d\t Unique Classes: %03d\n', layer, numel(u));
 
         close all;
     end
     fprintf('------------------------------------------------------\n');
+    fprintf(file_id,'------------------------------------------------------\n');
 
 end
 
