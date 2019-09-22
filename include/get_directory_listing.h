7c40933ac097cb2ef76dab8dbb2626dba1ac4461
Dave <davemers@iu.edu>
2019-09-21 23:44:01 -0400

    more tweaks to directory listing

--- include/get_directory_listing.h
+++ include/get_directory_listing.h
@@ -15,36 +15,41 @@ std::vector<std::string> get_directory_listing(std::string folder)
     std::vector<std::string> file_names;
     std::string search_path = folder + "*.*";
 
+#if defined(_WIN32) | defined(__WIN32__) | defined(__WIN32) | defined(_WIN64) | defined(__WIN64)
+
     WIN32_FIND_DATA fd;
     void* hFind = ::FindFirstFile(search_path.c_str(), &fd);
 
-#if defined(_WIN32) | defined(__WIN32__) | defined(__WIN32) | defined(_WIN64) | defined(__WIN64)
     if (hFind != INVALID_HANDLE_VALUE)
     {
         do
         {
             // read all (real) files in current folder
             // , delete '!' read other 2 default folder . and ..
-            if (!(fd.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY)) {
+            if (!(fd.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY)) 
+            {
                 file_names.push_back(fd.cFileName);
             }
         } while (::FindNextFile(hFind, &fd));
         ::FindClose(hFind);
     }
+
 #elif defined(__linux__) 
+// https://stackoverflow.com/questions/306533/how-do-i-get-a-list-of-files-in-a-directory-in-c
     DIR* dir;
     class dirent* ent;
     class stat st;
 
-    dir = opendir(directory.c_str());
-    while ((ent = readdir(dir)) != NULL) {
+    dir = opendir(folder.c_str());
+    while ((ent = readdir(dir)) != NULL) 
+    {
         const string file_name = ent->d_name;
-        const string full_file_name = directory + "/" + file_name;
+        //const string full_file_name = directory + "/" + file_name;
 
         if (file_name[0] == '.')
             continue;
 
-        if (stat(full_file_name.c_str(), &st) == -1)
+        if (stat((search_path + file_name).c_str(), &st) == -1)
             continue;
 
         const bool is_directory = (st.st_mode & S_IFDIR) != 0;
