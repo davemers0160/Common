d85c9b8c51c81edf124442d89cd2bbfe3f82f7f7
Emerson <1022450480@mil>
2019-12-09 15:57:34 -0500

    Update convert_groundtruth_to_dlib_input.m

--- matlab/dnn_utils/convert_groundtruth_to_dlib_input.m
+++ matlab/dnn_utils/convert_groundtruth_to_dlib_input.m
@@ -15,27 +15,30 @@ startpath = 'D:/Projects/';
 
 mat_file_filter = {'*.mat','Mat Files';'*.*','All Files' };
 
+% the input mat file should be a single table with all of the image file
+% names and rectangular bounding boxes
 [mat_save_file, mat_save_path] = uigetfile(mat_file_filter, 'Select Mat File', startpath);
 if(mat_save_path == 0)
     return;
 end
-data_table = load(fullfile(mat_save_path,mat_save_file));
-fields = fieldnames(data_table);
+load(fullfile(mat_save_path,mat_save_file));
+%fields = fieldnames(data);
 
-label_data = data_table.(fields{1});
+%label_data = data.(fields{1});
 
 commandwindow;
 
 %% convert the ground truth table to cell array and get the label names
-data = label_data.Variables;
 
-label_names = label_data.Properties.VariableNames;
+label_data = data.Variables;
+
+label_names = data.Properties.VariableNames;
+label_names = label_names(2:end);
 
 
 %% parse through the ground truth object detection table labels
 
-% write the data in the following format:
-% file location, {x,y,w,h,label}, {x,y,w,h,label},...
+% get the filename to write the data to based on the input mat file
 [~, tmp_file, ~] = fileparts(mat_save_file);
 file_name = fullfile(mat_save_path, strcat(tmp_file, '_input.txt'));
 file_id = fopen(file_name, 'w');
@@ -44,9 +47,11 @@ fprintf(file_id, '# Full data listing\n');
 fprintf(file_id, '# Data Directory:\n\n');
 fprintf(file_id, '# file location, {x,y,w,h,label}, {x,y,w,h,label}, ...\n');
 
-num_images = size(data,1);
-num_labels = size(data,2);
+num_images = size(label_data,1);
+num_labels = numel(label_names);
 
+% write the data in the following format:
+% file location, {x,y,w,h,label}, {x,y,w,h,label},...
 for idx=1:num_images
     
     % print out the image file name
