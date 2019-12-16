6d0bd80068b420247b4532ed87856f6253bd7c82
Dave <davemers@iu.edu>
2019-12-16 13:34:26 -0500

    working on getting the zed range estimation

--- python/ros_tf_obj_det.py
+++ python/ros_tf_obj_det.py
@@ -46,23 +46,27 @@ class RosTensorFlow():
         self.label_map = label_map_util.load_labelmap(PATH_TO_LABELS)
         self.categories = label_map_util.convert_label_map_to_categories(self.label_map, max_num_classes=NUM_CLASSES, use_display_name=True)
         self.category_index = label_map_util.create_category_index(self.categories)
-        
+
         self._cv_bridge = CvBridge()
 
         self.load_graph()
         self._session = tf.Session(graph=self.detection_graph)        
 
         # self._sub = rospy.Subscriber('image', Image, self.callback, queue_size=1)
-        self.image_sub = message_filters.Subscriber('image', Image, queue_size=1)
+        #self.image_sub = message_filters.Subscriber('image', Image, queue_size=1)
+        self.image_sub = message_filters.Subscriber('/zed/zed_node/rgb/image_rect_color', Image, queue_size=1)
         self.depth_sub = message_filters.Subscriber('/zed/zed_node/depth/depth_registered', Image, queue_size=1)
-        
+        self.cam_info_sub = rospy.Subscriber('/zed/zed_node/rgb/camera_info', CameraInfo)
+
         ts = message_filters.TimeSynchronizer([self.image_sub, self.depth_sub], 10)
         ts.registerCallback(self.callback)
-        
+
         self._img_pub = rospy.Publisher('obj_det/image', Image, queue_size=1)
         self._box_pub = rospy.Publisher('obj_det/boxes', String, queue_size=1)
-        
 
+        # get the camera info
+        get_camera_info()
+        
         #self.score_threshold = rospy.get_param('~score_threshold', 0.1)
         #self.use_top_k = rospy.get_param('~use_top_k', 5)
 
@@ -72,7 +76,7 @@ class RosTensorFlow():
 
         img_height = cv_image.shape[0]
         img_width  = cv_image.shape[1]
-                
+
         # Expand dimensions since the model expects images to have shape: [1, None, None, 3]
         image_np_expanded = np.expand_dims(cv_image, axis=0)
         image_tensor = self.detection_graph.get_tensor_by_name('image_tensor:0')
@@ -107,14 +111,23 @@ class RosTensorFlow():
                 #if(self.category_index[classes[idx]]['name'] == "Backpack"):
                 if(self.category_index[classes[idx]]['name'] == "Chair"):
                     bp_image = depth_img[y_min:y_max, x_min:x_max]
-                    img_crop = cv_image[y_min:y_max, x_min:x_max, :]
-                    rng_obj = cv2.mean(bp_image)
-                    print("Range (m): %2.4f" % (bp_image[int((x_max-x_min)/2),int((y_max-y_min)/2)]))
-#                    self._img_pub.publish(self._cv_bridge.cv2_to_imgmsg(img_crop, "rgb8"))
+                    avg_range = np.nanmean(bp_image)
+                    det_x = int((x_max-x_min)/2.0)
+                    det_y = int((y_max-y_min)/2.0)
+                    az = self.v_res*(det_x - int(self.img_w/2.0))
+                    el = self.h_res*(det_y - int(self.img_h/2.0))
+                    
+                    #img_crop = cv_image[y_min:y_max, x_min:x_max, :]
+                    
+                    print("Range: {}".format(avg_range))
+                    print("Az: {}".format(az))
+                    print("El: {}".format(el))
+                    #print("Range (m): %2.4f" % (bp_image[int((x_max-x_min)/2),int((y_max-y_min)/2)]))
+                    #self._img_pub.publish(self._cv_bridge.cv2_to_imgmsg(img_crop, "rgb8"))
                     self._img_pub.publish(self._cv_bridge.cv2_to_imgmsg(bp_image))
-                
-        box_string = box_string[:-2]        
-        
+
+        box_string = box_string[:-2]
+
         # Visualization of the results of a detection.
         vis_util.visualize_boxes_and_labels_on_image_array(
             cv_image,
@@ -124,13 +137,13 @@ class RosTensorFlow():
             self.category_index,
             use_normalized_coordinates=True,
             min_score_thresh=min_score,
-            line_thickness=8)        
-        
+            line_thickness=8)
+
 #        self._img_pub.publish(self._cv_bridge.cv2_to_imgmsg(cv_image, "rgb8"))
         self._box_pub.publish(box_string)
 
 
-    ## load in the detection graph from the frozen checkpoint file    
+    ## load in the detection graph from the frozen checkpoint file
     def load_graph(self):
         self.detection_graph = tf.Graph()
         with self.detection_graph.as_default():
@@ -139,7 +152,16 @@ class RosTensorFlow():
             serialized_graph = fid.read()
             od_graph_def.ParseFromString(serialized_graph)
             tf.import_graph_def(od_graph_def, name='')
-    
+
+    def get_camera_info(self):
+        self.img_h = self.cam_info_sub.height
+        self.img_w = self.cam_info_sub.width
+        self.h_res = 90.0/self.img_w
+        self.v_res = 60.0/self.img_h
+        print("{} x {}".format(self.img_h, self.img_w))
+        print("{}, {}".format(self.h_res, self.v_res))
+        
+        
     def main(self):
         rospy.spin()
 
