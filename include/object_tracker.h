8657e9c5a82e1d3be00050a643dbd0a69c9c144e
Dave <davemers@iu.edu>
2019-12-20 12:05:12 -0500

    moving files over the dlib-contrib

--- include/object_tracker.h
+++ include/object_tracker.h
@@ -6,11 +6,31 @@
 #include <string>
 
 
+template <typename image_type>
+struct detect
+{
+	uint64_t x;
+	uint64_t y;
+	uint64_t w;
+	uint64_t h;
+	
+	image_type image;
+	
+	std::string time_stamp;
+	
+	bool detection;
+}
+
 
 class object_tracker
 {
 
-
+private:
+	std::string uuid;
+	
+	
+public:
+	
 
 };	// end of object_detection
 
