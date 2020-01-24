e5c3c06f9c309e491b3a11bb17b8075284565411
Emerson <1022450480@mil>
2020-01-24 15:15:40 -0500

    Update get_current_time.h

--- include/get_current_time.h
+++ include/get_current_time.h
@@ -9,26 +9,68 @@
 // #include <fstream>
 #include <ctime>
 
-using namespace std;
 
+
+//std::string get_date(const struct tm &timeinfo)
+//{
+//
+//}
+
+// ----------------------------------------------------------------------------------------
+std::string get_date(const std::string &format)
+{
+    time_t rawtime;
+    struct tm *timeinfo; 
+    
+    char c_date[32];
+
+    time(&rawtime);
+    timeinfo = localtime(&rawtime);
+
+    strftime(c_date, 9, format.c_str(), timeinfo);
+
+    return (std::string)(c_date);
+
+}   // end of get_date
+
+// ----------------------------------------------------------------------------------------
+std::string get_time(const std::string &format)
+{
+    time_t rawtime;
+    struct tm *timeinfo;
+
+    char c_time[32];
+
+    time(&rawtime);
+    timeinfo = localtime(&rawtime);
+
+    strftime(c_time, 7, format.c_str(), timeinfo);
+
+    return (std::string)(c_time);
+
+}   // end of get_time
+
+// ----------------------------------------------------------------------------------------
 void get_current_time(std::string &sdate, std::string &stime)
 {
-	time_t rawtime;
-	struct tm * timeinfo;
+	//time_t rawtime;
+	//struct tm * timeinfo;
 
-	char c_date[9];
-	char c_time[7];
-	
-	time(&rawtime);
-	timeinfo = localtime(&rawtime);
+	//char c_date[9];
+	//char c_time[7];
+	//
+	//time(&rawtime);
+	//timeinfo = localtime(&rawtime);
 
-	strftime(c_date, 9, "%Y%m%d", timeinfo);
-	strftime(c_time, 7, "%H%M%S", timeinfo);
-	
-	sdate = (std::string)(c_date);
-	stime = (std::string)(c_time);
-	
-}	// end of getCurrentTime
+	//strftime(c_date, 9, "%Y%m%d", timeinfo);
+	//strftime(c_time, 7, "%H%M%S", timeinfo);
+	//
+	//sdate = (std::string)(c_date);
+	//stime = (std::string)(c_time);
 
+    sdate = get_date("%Y%m%d");
+    stime = get_time("%H%M%S");
+	
+}	// end of get_current_time
 
 #endif  // GET_CURRENT_TIME_H_
