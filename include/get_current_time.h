11e40b8943211d6e9aec7530ba3ade1d30d7ab6f
Dave <davemers@iu.edu>
2020-01-25 01:26:18 -0500

    revamping get current time

--- include/get_current_time.h
+++ include/get_current_time.h
@@ -8,24 +8,43 @@
 // #include <sstream>
 // #include <fstream>
 #include <ctime>
+#include <string>
 
-
-
-//std::string get_date(const struct tm &timeinfo)
-//{
-//
-//}
 
 // ----------------------------------------------------------------------------------------
-std::string get_date(const std::string &format)
+time_t convert_time(std::string stime)
 {
-    time_t rawtime;
+    // format: HHMMSS
+    struct tm timeinfo = { 0 };
+    timeinfo.tm_hour = std::stoi(stime.substr(0, 2));
+    timeinfo.tm_min = std::stoi(stime.substr(2, 2));
+    timeinfo.tm_sec = std::stoi(stime.substr(4, 2));
+
+    return mktime(&timeinfo);
+}
+
+// ----------------------------------------------------------------------------------------
+time_t convert_date(std::string sdate)
+{
+    // format: YYYYMMDD
+    struct tm timeinfo = { 0 };
+    timeinfo.tm_year = std::stoi(sdate.substr(0, 4)) - 1900;
+    timeinfo.tm_mon = std::stoi(sdate.substr(4, 2)) - 1;
+    timeinfo.tm_mday = std::stoi(sdate.substr(6, 2));
+
+    return mktime(&timeinfo);
+}
+
+// ----------------------------------------------------------------------------------------
+std::string get_date(time_t now, const std::string &format)
+{
+    //time_t rawtime;
     struct tm *timeinfo; 
     
     char c_date[32];
 
-    time(&rawtime);
-    timeinfo = localtime(&rawtime);
+    //time(&rawtime);
+    timeinfo = localtime(&now);
 
     strftime(c_date, 9, format.c_str(), timeinfo);
 
@@ -34,15 +53,15 @@ std::string get_date(const std::string &format)
 }   // end of get_date
 
 // ----------------------------------------------------------------------------------------
-std::string get_time(const std::string &format)
+std::string get_time(time_t now, const std::string &format)
 {
-    time_t rawtime;
+    //time_t rawtime;
     struct tm *timeinfo;
 
     char c_time[32];
 
-    time(&rawtime);
-    timeinfo = localtime(&rawtime);
+    //time(&rawtime);
+    timeinfo = localtime(&now);
 
     strftime(c_time, 7, format.c_str(), timeinfo);
 
@@ -53,7 +72,7 @@ std::string get_time(const std::string &format)
 // ----------------------------------------------------------------------------------------
 void get_current_time(std::string &sdate, std::string &stime)
 {
-	//time_t rawtime;
+	time_t now = time(NULL);
 	//struct tm * timeinfo;
 
 	//char c_date[9];
@@ -68,8 +87,8 @@ void get_current_time(std::string &sdate, std::string &stime)
 	//sdate = (std::string)(c_date);
 	//stime = (std::string)(c_time);
 
-    sdate = get_date("%Y%m%d");
-    stime = get_time("%H%M%S");
+    sdate = get_date(now, "%Y%m%d");
+    stime = get_time(now, "%H%M%S");
 	
 }	// end of get_current_time
 
