65976b56b1feb55e6f12e570252081fcc466b9c8
Emerson <1022450480@mil>
2020-02-13 11:22:48 -0500

    added win serial functions

--- include/linux_serial_fcns.h
+++ include/linux_serial_fcns.h
@@ -4,10 +4,10 @@
 
 #include <cstdint>
 
-#include <fcntl.h> 			// Contains file controls like O_RDWR
-#include <errno.h> 			// Error integer and strerror() function
-#include <termios.h> 		// Contains POSIX terminal control definitions
-#include <unistd.h> 		// write(), read(), close()
+#include <fcntl.h>             // Contains file controls like O_RDWR
+#include <errno.h>             // Error integer and strerror() function
+#include <termios.h>         // Contains POSIX terminal control definitions
+#include <unistd.h>         // write(), read(), close()
 
 #include <string>
 #include <vector>
@@ -19,116 +19,116 @@ class serial_port
 {
 
 private:
-	int port;
-	struct termios settings;
+    int port;
+    struct termios settings;
 
 //-----------------------------------------------------------------------------
-	void config(uint32_t baud_rate, uint32_t wait_time)
-	{
-		// get the current port configuration
-		tcgetattr(port, &settings);
+    void config(uint32_t baud_rate, uint32_t wait_time)
+    {
+        // get the current port configuration
+        tcgetattr(port, &settings);
 
-		// set the baud rate
-		cfsetispeed(&settings, baud_rate);
-		cfsetospeed(&settings, baud_rate);
+        // set the baud rate
+        cfsetispeed(&settings, baud_rate);
+        cfsetospeed(&settings, baud_rate);
 
-		settings.c_cflag &= ~PARENB;		// No Parity
-		settings.c_cflag &= ~CSTOPB;		// Stop bits = 1 
-		settings.c_cflag &= ~CSIZE;			// Clears the Mask
-		settings.c_cflag |=  CS8;			// Set the data bits = 8
+        settings.c_cflag &= ~PARENB;        // No Parity
+        settings.c_cflag &= ~CSTOPB;        // Stop bits = 1 
+        settings.c_cflag &= ~CSIZE;            // Clears the Mask
+        settings.c_cflag |=  CS8;            // Set the data bits = 8
 
-		// Turn off hardware based flow control (RTS/CTS).
-		settings.c_cflag &= ~CRTSCTS;
+        // Turn off hardware based flow control (RTS/CTS).
+        settings.c_cflag &= ~CRTSCTS;
 
-		// Turn on the receiver of the serial port (CREAD), other wise reading from the serial port will not work.
-		settings.c_cflag |= CREAD | CLOCAL;
+        // Turn on the receiver of the serial port (CREAD), other wise reading from the serial port will not work.
+        settings.c_cflag |= CREAD | CLOCAL;
 
-		// Turn off software based flow control (XON/XOFF).
-		settings.c_iflag &= ~(IXON | IXOFF | IXANY);
+        // Turn off software based flow control (XON/XOFF).
+        settings.c_iflag &= ~(IXON | IXOFF | IXANY);
 
-		// Setting the mode of operation,the default mode of operation of serial port in 
-		// Linux is the Cannonical mode. For Serial communications with outside devices 
-		// like serial modems, mice etc NON Cannonical mode is recommended.
-		settings.c_iflag &= ~(ICANON | ECHO | ECHOE | ISIG);
-		
-		// Prevent special interpretation of output bytes (e.g. newline chars)
-		settings.c_oflag &= ~(OPOST | ONLCR); 
+        // Setting the mode of operation,the default mode of operation of serial port in 
+        // Linux is the Cannonical mode. For Serial communications with outside devices 
+        // like serial modems, mice etc NON Cannonical mode is recommended.
+        settings.c_iflag &= ~(ICANON | ECHO | ECHOE | ISIG);
+        
+        // Prevent special interpretation of output bytes (e.g. newline chars)
+        settings.c_oflag &= ~(OPOST | ONLCR); 
 
-		// set wait time
-		settings.c_cc[VTIME] = wait_time;    // Wait for up to 1s (100ms increments), returning as soon as any data is received.
-		settings.c_cc[VMIN] = 0;
-		
-		// Save tty settings, also checking for error
-		int res = tcsetattr(port, TCSANOW, &settings);
-		if (res != 0) 
-			throw std::runtime_error("Error from tcsetattr: " + strerror(errno));
-		
-	}
+        // set wait time
+        settings.c_cc[VTIME] = wait_time;    // Wait for up to 1s (100ms increments), returning as soon as any data is received.
+        settings.c_cc[VMIN] = 0;
+        
+        // Save tty settings, also checking for error
+        int res = tcsetattr(port, TCSANOW, &settings);
+        if (res != 0) 
+            throw std::runtime_error("Error from tcsetattr: " + strerror(errno));
+        
+    }
 
 //-----------------------------------------------------------------------------
 public:
 
-	serial_port() = default;
-	
+    serial_port() = default;
+    
 //-----------------------------------------------------------------------------
-	void open(std::string named_port, uint32_t baud_rate, uint32_t wait_time)
-	{
-		port = open(named_port.c_str(), O_RDWR | O_NOCTTY);
-		
-		if(port == 1)
-		{
-			throw std::runtime_error("Error opening port: " + named_port);
-			return;
-		}
-		
-		config(baud_rate, wait_time);
-		
-	}
+    void open(std::string named_port, uint32_t baud_rate, uint32_t wait_time)
+    {
+        port = open(named_port.c_str(), O_RDWR | O_NOCTTY);
+        
+        if(port == 1)
+        {
+            throw std::runtime_error("Error opening port: " + named_port);
+            return;
+        }
+        
+        config(baud_rate, wait_time);
+        
+    }
 
 //-----------------------------------------------------------------------------
-	void read(std::vector<uint8_t> &read_bufffer, uint64_t count)
-	{
-		read_bufffer.clear();
-		read_bufffer.resize(count);
-		
-		int num_bytes = read(port, &read_bufffer[0], count);
-		
-		if(num_bytes != count)
-		{
-			throw std::runtime_error("Wrong number of bytes received. Expected: " + count.to_string() + ", received: " + num_bytes.to_string());
-			return;			
-		}
-			
-	}
-	
-	void read(std::string &read_bufffer, uint64_t count)
-	{
-		std::vector<uint8_t> rb(count);
-		
-		read(rb,  count);
-					
-		read_bufffer.assign(rb.begin(), rb.end());
-				
-	}
+    void read(std::vector<uint8_t> &read_bufffer, uint64_t count)
+    {
+        read_bufffer.clear();
+        read_bufffer.resize(count);
+        
+        int num_bytes = read(port, &read_bufffer[0], count);
+        
+        if(num_bytes != count)
+        {
+            throw std::runtime_error("Wrong number of bytes received. Expected: " + count.to_string() + ", received: " + num_bytes.to_string());
+            return;
+        }
+            
+    }
+    
+    void read(std::string &read_bufffer, uint64_t count)
+    {
+        std::vector<uint8_t> rb(count);
+        
+        read(rb,  count);
+                    
+        read_bufffer.assign(rb.begin(), rb.end());
+                
+    }
 
 //-----------------------------------------------------------------------------
-	void write(std::vector<uint8_t> write_buffer)
-	{
-		int bytes_written = write(port, write_buffer, write_buffer.size());		
-	}
-	
-	void write(std::string write_buffer)
-	{
-		int bytes_written = write(port, write_buffer.c_str(), write_buffer.length());
-	}
-	
+    void write(std::vector<uint8_t> write_buffer)
+    {
+        int bytes_written = write(port, write_buffer.data(), write_buffer.size());        
+    }
+    
+    void write(std::string write_buffer)
+    {
+        int bytes_written = write(port, write_buffer.c_str(), write_buffer.length());
+    }
+    
 //-----------------------------------------------------------------------------
-	void close()
-	{
-		close(port);
-		port = NULL;
-	}
+    void close()
+    {
+        close(port);
+        port = NULL;
+    }
 
-};	// end of class
+};    // end of class
 
-#endif	// _LINUX_SERIAL_CTRL_H_
+#endif    // _LINUX_SERIAL_CTRL_H_
---
+++ include/win_serial_fcns.h
@@ -0,0 +1,142 @@
+#ifndef _WINDOWS_SERIAL_CTRL_H_
+#define _WINDOWS_SERIAL_CTRL_H_
+
+#include <windows.h>
+
+#include <cstdint>
+#include <string>
+#include <vector>
+#include <stdexcept>
+
+// https://www.codeguru.com/cpp/i-n/network/serialcommunications/article.php/c5425/Serial-Communication-in-Windows.htm
+
+//-----------------------------------------------------------------------------
+class serial_port
+{
+
+private:
+    HANDLE port;
+    DCB settings = {0};
+    
+//-----------------------------------------------------------------------------
+    void config(uint32_t baud_rate, uint32_t wait_time)
+    {
+        settings.DCBlength = sizeof(settings);
+
+        //After that retrieve the current settings of the serial port using the GetCommState() function.
+        bool Status = GetCommState(port, &settings);
+
+        // set the values for Baud rate, Byte size, Number of start/Stop bits etc.
+        settings.BaudRate = baud_rate;                              // Setting BaudRate
+        settings.ByteSize = 8;                                      // Setting ByteSize = 8
+        settings.StopBits = ONESTOPBIT;                             // Setting StopBits = 1
+        settings.Parity   = NOPARITY;                               // Setting Parity = None
+        
+        COMMTIMEOUTS timeouts = { 0 };
+        timeouts.ReadIntervalTimeout         = wait_time*100;       // in milliseconds
+        timeouts.ReadTotalTimeoutConstant    = wait_time*100;       // in milliseconds
+        timeouts.ReadTotalTimeoutMultiplier  = wait_time*100;       // in milliseconds
+        timeouts.WriteTotalTimeoutConstant   = wait_time*100;       // in milliseconds
+        timeouts.WriteTotalTimeoutMultiplier = wait_time*100;       // in milliseconds
+        SetCommTimeouts (port, &timeouts);
+        
+        SetCommState(port, &settings);
+    }    
+
+//-----------------------------------------------------------------------------
+public:
+
+    serial_port() = default;
+    
+//-----------------------------------------------------------------------------
+    void open(std::string named_port, uint32_t baud_rate, uint32_t wait_time)
+    {
+        //  Open a handle to the specified com port: expect COMX
+        named_port = "\\\\.\\" + named_port;
+        port = ::CreateFile(named_port.c_str(),
+              GENERIC_READ | GENERIC_WRITE,
+              0,                            //  must be opened with exclusive-access
+              NULL,                         //  default security attributes
+              OPEN_EXISTING,                //  must use OPEN_EXISTING
+              0,                            //  not overlapped I/O
+              NULL );                       //  hTemplate must be NULL for comm devices
+              
+        if(port == INVALID_HANDLE_VALUE)
+        {
+            throw std::runtime_error("Error opening port: " + named_port);
+            return;
+        }
+    }   // end of open
+
+
+//-----------------------------------------------------------------------------
+    void read(std::vector<uint8_t> &read_bufffer, uint64_t count)
+    {
+        DWORD num_bytes = 0;                        // Number of bytes read from the port
+        DWORD event_mask = 0; 
+
+        read_bufffer.clear();
+        read_bufffer.resize(count);
+        
+        // set the events to be monitored for a communication device: EV_TXTEMPTY|EV_RXCHAR
+        bool Status = SetCommMask(port, EV_RXCHAR);
+        
+        // wait for the events set by SetCommMask() to happen
+        Status = WaitCommEvent(port, &event_mask, NULL); 
+        
+        ReadFile(port,                              // Handle of the Serial port
+                &read_bufffer[0],                   // Buffer to store the data
+                count,                              // Number of bytes to read in
+                &num_bytes,                         //Number of bytes read
+                NULL);
+             
+        if(num_bytes != count)
+        {
+            throw std::runtime_error("Wrong number of bytes received. Expected: " + count.to_string() + ", received: " + num_bytes.to_string());
+            return;
+        }        
+    }
+
+    void read(std::string &read_bufffer, uint64_t count)
+    {
+        std::vector<uint8_t> rb(count);
+        
+        read(rb,  count);
+                    
+        read_bufffer.assign(rb.begin(), rb.end());            
+    }
+  
+//-----------------------------------------------------------------------------
+    void write(std::vector<uint8_t> write_buffer)
+    {
+        DWORD bytes_written = 0;                    // Number of bytes written to the port
+        
+        bool Status = WriteFile(port,               // Handle to the Serial port
+                        write_buffer.data(),        // Data to be written to the port
+                        write_buffer.size(),        // No of bytes to write
+                        &bytes_written,             // Bytes written
+                        NULL);
+
+    }
+    
+    void write(std::string write_buffer)
+    {
+        DWORD bytes_written = 0;                    // Number of bytes written to the port
+        
+        bool Status = WriteFile(port,               // Handle to the Serial port
+                        write_buffer.c_str(),       // Data to be written to the port
+                        write_buffer.length(),      // No of bytes to write
+                        &bytes_written,             // Bytes written
+                        NULL);    
+    }
+  
+//-----------------------------------------------------------------------------
+    void close()
+    {
+        CloseHandle(port);
+        port = NULL;
+    }
+
+};    // end of class
+    
+#endif  // _WINDOWS_SERIAL_CTRL_H_
