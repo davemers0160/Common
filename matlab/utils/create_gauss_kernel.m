b4c46b90f627bb01ff65ef554b25ded2146e25d5
Dave <davemers@iu.edu>
2020-04-14 13:54:05 -0400

    update to gaussian kernel

--- matlab/utils/create_gauss_kernel.m
+++ matlab/utils/create_gauss_kernel.m
@@ -2,7 +2,7 @@ function [kernel] = create_gauss_kernel(kernel_size, sigma)
 
 	for row = 1:kernel_size
 		for col = 1:kernel_size	
-            scale = 1.0/(2*pi*sigma*sigma);
+            scale = 1.0/(sigma*sqrt(2*pi));
             x = ((col-1) - floor(kernel_size/2));
             y = ((row-1) - floor(kernel_size/2));
 			kernel(row, col) = scale * exp((-x*x - y*y) / (2 * sigma*sigma));
