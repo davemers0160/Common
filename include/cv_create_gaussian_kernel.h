34c63c3075836d7d46ac725205fba5eb7d63ffce
Dave <davemers@iu.edu>
2020-09-24 22:46:57 -0400

    adding create gaussian kernel

---
+++ include/cv_create_gaussian_kernel.h
@@ -0,0 +1,41 @@
+#ifndef CREATE_GAUSSIAN_KERNEL_H_
+#define CREATE_GAUSSIAN_KERNEL_H_
+
+#include <cstdint>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <string>
+
+// OpenCV Includes
+#include <opencv2/core/core.hpp>           
+#include <opencv2/highgui/highgui.hpp>     
+#include <opencv2/imgproc/imgproc.hpp>  
+
+
+void create_gaussian_kernel(uint32_t size, double sigma, cv::Mat &kernel)
+{
+	// assumes a 0 mean Gaussian distribution
+	uint32_t row, col;
+    double s = sigma*sigma;
+
+    kernel = cv::Mat::zeros(size, size, CV_64FC1);
+
+    double t = (1.0 / (2 * CV_PI *s));
+
+	for (row = 0; row < size; ++row)
+	{
+		for (col = 0; col < size; ++col)
+		{
+			kernel.at<double>(row, col) = t * std::exp((-((col - (size >> 1))*(col - (size >> 1))) - ((row - (size >> 1))*(row - (size >> 1)))) / (2 * s));
+		}
+	}
+
+	double matsum = (double)cv::sum(kernel)[0];
+
+	kernel = kernel * (1.0 / matsum);	// get the matrix to sum up to 1...
+
+
+}	// end of createGaussKernel
+
+#endif  // CREATE_GAUSSIAN_KERNEL_H_
--- matlab/dnn_utils/get_color_stats.m
+++ matlab/dnn_utils/get_color_stats.m
@@ -45,28 +45,44 @@ data_directory = params{1}{1};
 params(1) = [];
 
 %% parse through the data
+num_images = length(params);
+hist_bins = 256;
 
 red = 0;
 green = 0;
 blue = 0;
 gray = 0;
 
-for idx=1:length(params)    
+r_hist = zeros(1, hist_bins);
+g_hist = zeros(1, hist_bins);
+b_hist = zeros(1, hist_bins);
+gr_hist = zeros(1, hist_bins);
+
+for idx=1:num_images    
 
     % this is expected to be a 3-channel color image
     img = imread(fullfile(data_directory, params{idx}{file_index}));
 %     img = img(:,:,1:3);
+    img_size = size(img,1) * size(img,2);
 
     if(size(img,3) == 1)
         r = mean(mean(img));
         g = mean(mean(img));
         b = mean(mean(img));
-        gr = mean(mean(img));     
+        gr = mean(mean(img));
+        
     else
+        gr_img = rgb2gray(img);
         r = mean(mean(img(:,:,1)));
         g = mean(mean(img(:,:,2)));
         b = mean(mean(img(:,:,3)));
-        gr = mean(mean(rgb2gray(img)));
+        gr = mean(mean(gr_img));
+        
+        r_hist = r_hist + (histcounts(img(:,:,1), hist_bins)/img_size);
+        g_hist = g_hist + (histcounts(img(:,:,2), hist_bins)/img_size);
+        b_hist = b_hist + (histcounts(img(:,:,3), hist_bins)/img_size);
+        gr_hist = gr_hist + (histcounts(gr_img, hist_bins)/img_size);
+        
     end
     
     red = red + r;
@@ -78,10 +94,15 @@ for idx=1:length(params)
 
 end
 
-red = red/length(params);
-green = green/length(params);
-blue = blue/length(params);
-gray = gray/length(params);
+red = red/num_images;
+green = green/num_images;
+blue = blue/num_images;
+gray = gray/num_images;
+
+r_hist = r_hist/num_images;
+g_hist = g_hist/num_images;
+b_hist = b_hist/num_images;
+gr_hist = gr_hist/num_images;
 
 fprintf('red:   %3.4f\n', red);
 fprintf('green: %3.4f\n', green);
@@ -89,5 +110,19 @@ fprintf('blue:  %3.4f\n', blue);
 fprintf('gray:  %3.4f\n', gray);
 
 fprintf('%8.4f, %8.4f, %8.4f, %8.4f\n', red, green, blue, gray);
+
+%% calculate the histograms CDF
+
+gr_cdf = zeros(1, hist_bins);
+
+for idx=1:hist_bins    
+    gr_cdf(1,idx) = sum(gr_hist(1,1:idx));
+    fprintf('%d, ', floor(gr_cdf(1,idx)*(hist_bins-1)));
+end
+
+fprintf('\n');
+
+
+
 
 
