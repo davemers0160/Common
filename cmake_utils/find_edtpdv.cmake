f86cfecd0fd7c0c85d4d2089d571d0b4cee61ffb
Dave <davemers@iu.edu>
2020-10-25 21:24:11 -0400

    adding checks for linux vs windows

--- cmake_utils/find_edtpdv.cmake
+++ cmake_utils/find_edtpdv.cmake
@@ -2,14 +2,22 @@ message(STATUS "----------------------------------------------------------------
 message(STATUS "Looking for EDT PDV library...")
 
 find_path(EDTPDV_INCLUDE_DIRS edtinc.h
-    PATHS /usr/local /opt/edt "C:/Program Files/EDT/PDV"  "C:/EDT/PDV" ENV CPATH 
+    PATHS /usr/local /opt/EDTpdv "C:/Program Files/EDT/PDV"  "C:/EDT/PDV" ENV CPATH 
     )
 
+if(WIN32)
 find_library(EDTPDV_LIBS pdvlib
     HINTS ${EDTPDV_INCLUDE_DIRS}
-    PATHS /usr/local /opt/edt "C:/Program Files/EDT/PDV"  "C:/EDT/PDV"
+    PATHS "C:/Program Files/EDT/PDV"  "C:/EDT/PDV"
+    PATH_SUFFIXES bin/amd64 
+    )
+else()
+find_library(EDTPDV_LIBS pdv
+    HINTS ${EDTPDV_INCLUDE_DIRS}
+    PATHS /usr/local /opt/EDTpdv
     PATH_SUFFIXES lib bin/amd64 lib64 x64 
     )
+endif()
     
 mark_as_advanced(EDTPDV_LIBS EDTPDV_INCLUDE_DIRS)
 
