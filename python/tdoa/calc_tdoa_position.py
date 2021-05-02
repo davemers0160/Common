c4fe168354d92b120e0873f9021c3f6fbc8336ab
Dave <davemers@iu.edu>
2021-05-02 02:07:08 -0400

    adding T to the code

--- python/tdoa/calc_tdoa_position.py
+++ python/tdoa/calc_tdoa_position.py
@@ -1,7 +1,7 @@
 import math
 import numpy as np
 
-def calc_tdoa_position(S, Po, v):
+def calc_tdoa_position(S, T, Po, v):
     # Inputs: S - Numpy array of positions [[x1,x2,...,t], [x1,x2,...,t]]
     #         Po - Intial guess position (x,y,z)
     #         v - speed of the signal
@@ -16,15 +16,13 @@ def calc_tdoa_position(S, Po, v):
     max_iter = 50
     iter = 0
 
-    # get the number of measurements
+    # get the number of measurements and the number of dimensions
     N, num_dim = S.shape
-    num_dim -= 1
 
     # sort the S array in terms of the shortest to longest times
-    
-    index = np.argsort(S[:, -1])
+    index = np.argsort(T)
     S = S[index]
-    
+    T = T[index]
 
     while((iter < max_iter) and (err > d_err)):
 
@@ -32,14 +30,14 @@ def calc_tdoa_position(S, Po, v):
         R = np.zeros([N,1], dtype=float)
         for idx in range(0, N):
             #R(idx) = sqrt((S(idx,1) - Po(1))^2 + (S(idx,2) - Po(2))^2 + (S(idx,3) - Po(3))^2)
-            R[idx] = math.sqrt(np.sum((S[idx, 0:-1] - Po) * (S[idx, 0:-1] - Po)))
+            R[idx] = math.sqrt(np.sum((S[idx] - Po) * (S[idx] - Po)))
 
         # build A and b
         A = np.zeros([N-1, num_dim], dtype=float)
         b = np.zeros([N-1, 1], dtype=float)
         for idx in range(1, N):
-            A[idx - 1, :] = (S[idx, 0:-1] - Po)/R[idx] - (S[0, 0:-1] - Po)/R[0]
-            b[idx - 1] = v * (S[idx, -1] - S[0, -1]) - (R[idx] - R[0])
+            A[idx - 1, :] = (S[idx] - Po)/R[idx] - (S[0] - Po)/R[0]
+            b[idx - 1] = v * (T[idx] - T[0]) - (R[idx] - R[0])
             
         # invert A -> (AtA)^-1 At
         dP = np.matmul(np.matmul(np.linalg.pinv(np.matmul(A.transpose(), A)), A.transpose()), b).transpose()
--- python/tdoa/tdoa_bokeh_test.py
+++ python/tdoa/tdoa_bokeh_test.py
@@ -11,17 +11,20 @@ from bokeh.layouts import column, row, Spacer
 
 from calc_tdoa_position import calc_tdoa_position
 
-# <script type="text/javascript" src="https://cdn.bokeh.org/bokeh/release/bokeh-api-2.3.0.min.js" integrity="sha384-RMPdnxafNybXTSOEnNc5DcUZuWp5AI7/X1sevmORhTwgIBG9mS7D1mQ0Fbo2CvCs" crossorigin="anonymous"></script>
-# <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/mathjs/9.3.2/math.js" integrity="sha512-Imer9iTeuCPbyZUYNTKsBWsAk3m7n1vOgPsAmw4OlkGSS9qK3/WlJZg7wC/9kL7nUUOyb06AYS8DyYQV7ELEbg==" crossorigin="anonymous"></script>
-
+"""
+<script type="text/javascript" src="https://cdn.bokeh.org/bokeh/release/bokeh-api-2.3.0.min.js" integrity="sha384-RMPdnxafNybXTSOEnNc5DcUZuWp5AI7/X1sevmORhTwgIBG9mS7D1mQ0Fbo2CvCs" crossorigin="anonymous"></script>
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/mathjs/9.3.2/math.js" integrity="sha512-Imer9iTeuCPbyZUYNTKsBWsAk3m7n1vOgPsAmw4OlkGSS9qK3/WlJZg7wC/9kL7nUUOyb06AYS8DyYQV7ELEbg==" crossorigin="anonymous"></script>
+"""
 # speed of the signal
 v = 299792458
 
 # predfine the stations
-S = 1*np.array([[4, 5, 0],
-              [1, 2, 0],
-              [-3, 2, 0]], dtype='float')
+S = 1*np.array([[4, 5],
+              [1, 2],
+              [-3, 2]], dtype='float')
 
+# time of arrival
+T = np.array([0, 0, 0], dtype='float')
 
 # define the location of the target
 P = 1*np.array([0, 6], dtype='float')
@@ -30,7 +33,6 @@ P = 1*np.array([0, 6], dtype='float')
 Po = 1*np.array([0.333, 3], dtype='float')
 
 N, num_dim = S.shape
-num_dim = num_dim - 1
 
 # estimating +/- 10 error
 range_err = 0.01
@@ -40,11 +42,11 @@ time_err = 0.0000000001
 
 ### ---------------------------------------------------------------------------
 # calculate the arrival times
-def calc_arrival_times(S, P, N, v):
+def calc_arrival_times(S, T, P, N, v):
     for idx in range(0, N):
-        S[idx, -1] = math.sqrt(np.sum((S[idx, 0:-1] - P)*(S[idx, 0:-1] - P)))/v
+        T[idx] = math.sqrt(np.sum((S[idx] - P)*(S[idx] - P)))/v
 
-    return S
+    return T
 
 ### ---------------------------------------------------------------------------
 # calculate the covariance matrix and aou circle
@@ -73,7 +75,7 @@ def calc_covariance_matrix(P_new, cp, num_trials):
 
 ### ---------------------------------------------------------------------------
 # tdoa_source = ColumnDataSource(data=dict(tx=[P[0]], ty=[P[1]], sx=S[:,0], sy=S[:,1], pox=[Po[0]], poy=[Po[1]], v=[v]))
-st_source = ColumnDataSource(data=dict(x=(S[:, 0]*1), y=(S[:, 1]*1), t=S[:, 2], S=S*1))
+st_source = ColumnDataSource(data=dict(x=(S[:, 0]*1), y=(S[:, 1]*1), t=T, s=S*1))
 ig_source = ColumnDataSource(data=dict(x=[Po[0]*1], y=[Po[1]*1]))
 tx_source = ColumnDataSource(data=dict(x=[P[0]*1], y=[P[1]*1]))
 etx_source = ColumnDataSource(data=dict(x=[P[0]*1], y=[P[1]*1]))
@@ -82,7 +84,7 @@ ell_source = ColumnDataSource(data=dict(x=[P[0]*1], y=[P[1]*1]))
 
 
 ### ---------------------------------------------------------------------------
-tdoa_dict = dict(st=st_source, ig=ig_source, ctx=ctx_source, tx=tx_source, etx=etx_source, N=N, v=v, range_err=range_err, time_err=time_err)
+tdoa_dict = dict(st=st_source, ig=ig_source, ctx=ctx_source, tx=tx_source, etx=etx_source, ell=ell_source, N=N, D=num_dim, v=v, range_err=range_err, time_err=time_err)
 update_plot_callback = CustomJS(args=tdoa_dict, code="""
 
     console.log('test')
@@ -91,7 +93,8 @@ update_plot_callback = CustomJS(args=tdoa_dict, code="""
     var st = st.data;
     
     var v = v;
-    var S = [];
+    var S = []; //st['s'];
+    var T = [];
     var Po = [ig.data['x'][0], ig.data['y'][0] ];
     var P = [tx.data['x'][0], tx.data['y'][0] ];
     
@@ -168,9 +171,15 @@ update_plot_callback = CustomJS(args=tdoa_dict, code="""
         return value/length;
     }
     
+    //--------------------------------------------------------------------
+    function calc_covariance(P)
+    {
+        var Pc = math.multiply(math.transpose(P), P, 1/P.length)
+    
+    }
     
     //--------------------------------------------------------------------
-    function calc_tdoa_position(S, Po, v)
+    function calc_tdoa_position(S, T, Po, v)
     {
         // error limit
         var d_err = 1e-3;
@@ -179,10 +188,19 @@ update_plot_callback = CustomJS(args=tdoa_dict, code="""
         // iteration limit
         var max_iter = 50;
         var iter = 0;
-    
+        
+        // copy the initial guess
         var Pn = Po.slice(0);
         
-        S.sort(function(a,b){ return a[2] > b[2] ? 1 : -1; })
+        // get the min time value and swap the values for S and T
+        var t_index = T.indexOf(Math.min(...T)) 
+        if(t_index != 0)
+        {
+            // swap index and 0
+            [T[0], T[t_index]] = [T[t_index], T[0]];
+            [S[0], S[t_index]] = [S[t_index], S[0]];
+        }
+        //S.sort(function(a,b){ return a[2] > b[2] ? 1 : -1; })
         
         //--------------------------------------------------------------------
         while((iter < max_iter) && (err > d_err))
@@ -201,30 +219,33 @@ update_plot_callback = CustomJS(args=tdoa_dict, code="""
             {
                 A[idx - 1][0] = (S[idx][0] - Pn[0])/R[idx] - (S[0][0] - Pn[0])/R[0];
                 A[idx - 1][1] = (S[idx][1] - Pn[1])/R[idx] - (S[0][1] - Pn[1])/R[0];
-                b[idx - 1] = v * (S[idx][2] - S[0][2]) - (R[idx] - R[0]);
+                b[idx - 1] = v * (T[idx] - T[0]) - (R[idx] - R[0]);
             }
                 
             // invert A -> (AtA)^-1 At
-            var AT = mat_trans(A);
+            //var AT = mat_trans(A);
     
             // multiply ATA
-            var ATA = mat_mul(AT, A);
+            //var ATA = mat_mul(AT, A);
             
-            ATA = inv_mat(ATA);
+            //ATA = inv_mat(ATA);
             
-            ATA = mat_mul(ATA, AT);
+            //ATA = mat_mul(ATA, AT);
             
             // ATA*b
-            var dP = [];
-            dP[0] = ATA[0][0]*b[0] + ATA[0][1]*b[1];
-            dP[1] = ATA[1][0]*b[0] + ATA[1][1]*b[1];
+            //var dP = [];
+            //dP[0] = ATA[0][0]*b[0] + ATA[0][1]*b[1];
+            //dP[1] = ATA[1][0]*b[0] + ATA[1][1]*b[1];
+            var dP = math.multiply(math.multiply(math.inv(math.multiply(math.transpose(A), A)), math.transpose(A)), b)
             
             // generate new Po: Po = Po - dP
-            Pn[0] = Pn[0] - dP[0];
-            Pn[1] = Pn[1] - dP[1];
+            //Pn[0] = Pn[0] - dP[0];
+            //Pn[1] = Pn[1] - dP[1];
+            Pn = math.subtract(Pn, dP);
     
             // get the error
-            err = Math.sqrt(dP[0]*dP[0] + dP[1]*dP[1]); 
+            //err = Math.sqrt(dP[0]*dP[0] + dP[1]*dP[1]);
+            err = math.sqrt(math.multiply(math.transpose(dP), dP));
                    
             ++iter;
         }
@@ -248,33 +269,54 @@ update_plot_callback = CustomJS(args=tdoa_dict, code="""
         S[idx] = [];
         S[idx][0] = st['x'][idx];
         S[idx][1] = st['y'][idx];
-        S[idx][2] = Math.sqrt( (S[idx][0] - P[0])*(S[idx][0] - P[0]) + (S[idx][1] - P[1])*(S[idx][1] - P[1]) )/v;
+        T[idx] = Math.sqrt( (S[idx][0] - P[0])*(S[idx][0] - P[0]) + (S[idx][1] - P[1])*(S[idx][1] - P[1]) )/v;
     }
       
     var P_new = [];
     var Sn = [];
+    var Tn = [];
     
     for(var idx=0; idx<num_trials; ++idx)
     {
     
         for(var jdx=0; jdx<N; ++jdx)
         {
-            Sn[jdx] = [S[jdx][0] + range_err*randn(), S[jdx][1] + range_err*randn(), S[jdx][2] + time_err*randn()];
+            Sn[jdx] = [S[jdx][0] + range_err*randn(), S[jdx][1] + range_err*randn()];
             //Sn[jdx][1] = S[jdx][1] + range_err*randn();
             //Sn[jdx][2] = S[jdx][2] + time_err*randn();
+            Tn[jdx] = T[jdx] + time_err*randn()
         }    
     
         // calculate the position
-        P_new[idx] = calc_tdoa_position(Sn, Po, v);
+        P_new[idx] = calc_tdoa_position(Sn, Tn, Po, v);
         
     }
     
-    var PT = mat_trans(P_new);
-    var PTP = mat_mul(PT, P_new);
-    
+    //var PT = mat_trans(P_new);
+    //var PTP = mat_mul(PT, P_new);
+    // get the center of the locations
     var cx = arr_avg(P_new.map(function(value,index) { return value[0]; }) );
     var cy = arr_avg(P_new.map(function(value,index) { return value[1]; }) );
     
+    // calculate the covariance matrix  
+    var Pc = math.multiply(math.transpose(P_new), P_new, 1/P_new.length);
+    
+    // get the eigen values and vectors
+    var p_eigs = math.eigs(Pc);
+    
+    // get the confidence interval
+    var scale = -2 * Math.log10(1 - 0.95)
+    p_eigs.values = math.multiply(p_eigs.values, scale);
+    
+    // set the ellipse plotting segments
+    var theta = math.range(0, 2*Math.PI, 2*Math.PI/100);
+
+    // calculate the ellipse
+    var r_ellipse = math.multiply(math.multiply(p_eigs.vectors, math.sqrt(math.diag(p_eigs.values))), math.matrix([math.cos(theta), math.sin(theta)]));
+    var r_x = math.add(math.row(r_ellipse,0), cx);
+    var r_y = math.add(math.row(r_ellipse,1), cy);
+    
+
     // return the results   
     console.log(Po);
     ctx.data['x'] = [cx];
@@ -283,8 +325,12 @@ update_plot_callback = CustomJS(args=tdoa_dict, code="""
     etx.data['x'] = P_new.map(function(value,index) { return value[0]; });
     etx.data['y'] = P_new.map(function(value,index) { return value[1]; });
     
+    ell.data['x'] = r_x._data[0];
+    ell.data['y'] = r_y._data[0];
+    
     ctx.change.emit();
     etx.change.emit();
+    ell.change.emit();
     
     var bp = 1;
 """)
@@ -319,7 +365,7 @@ st_datatable = DataTable(source=st_source, columns=columns, width=250, height=12
 # define the initial guess
 ig_datatable = DataTable(source=ig_source, columns=columns, width=250, height=100, editable=True)
 
-S = calc_arrival_times(S, P, N, v)
+T = calc_arrival_times(S, T, P, N, v)
 
 #P_new[idx], iter[idx], err[idx] = calc_tdoa_position(Sn[:, :, idx], Po, v)
 
@@ -329,13 +375,13 @@ num_trials = 100
 P_new = np.zeros([num_trials, num_dim], dtype='float')
 iter = np.zeros([num_trials, 1], dtype='float')
 err = np.zeros([num_trials, 1], dtype='float')
-Sn = np.zeros([N, num_dim+1, num_trials], dtype='float')
+Sn = np.zeros([N, num_dim, num_trials], dtype='float')
 
 #P_new(idx,:), iter(idx,:), err(idx,:)]= calc_3d_tdoa_position(Sn(:,:, idx), Po, v)
 for idx in range(0, num_trials):
-    Sn[:, 0:-1, idx] = S[:, 0:-1] + np.random.normal(0, range_err, size=(N, num_dim))
-    Sn[:, -1, idx] = S[:, -1] + np.random.normal(0, time_err, size=(N))
-    P_new[idx], iter[idx], err[idx] = calc_tdoa_position(Sn[:, :, idx], Po, v)
+    Sn[:, :, idx] = S + np.random.normal(0, range_err, size=(N, num_dim))
+    Tn = T + np.random.normal(0, time_err, size=(N))
+    P_new[idx], iter[idx], err[idx] = calc_tdoa_position(Sn[:, :, idx], Tn, Po, v)
 
 # get the center/means in each direction
 cp = np.mean(P_new, axis=0)
