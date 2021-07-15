e5523563e1d775636297d27802255251d38a588e
Dave <davemers@iu.edu>
2021-07-15 13:39:40 -0400

    update to blues colormap

--- matlab/blues.m
+++ matlab/blues.m
@@ -2,9 +2,9 @@ function rgb = blues(n)
 
     index = (0:1:n-1)*(12/(n-1));
     
-    r = -0.0724*index + 0.9;
-    g = -0.0541*index + 0.92;
-    b = -0.0350*index + 1.0;
+    r = -0.0724*index + 0.93;
+    g = -0.0541*index + 0.95;
+    b = -0.0350*index + 1.00;
 
     rgb = cat(2,r',g',b');
 end
