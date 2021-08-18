fea86b500a6a97baa29be5067e16624f572f0518
Dave <davemers@iu.edu>
2021-08-17 22:01:47 -0400

    getting everything to work in linux

--- include/mmap.h
+++ include/mmap.h
@@ -7,21 +7,19 @@
 
 #if defined(_WIN32) | defined(__WIN32__) | defined(__WIN32) | defined(_WIN64) | defined(__WIN64)
 #include <windows.h>
-//#pragma comment(lib, "user32.lib")
+
 #else
-#include <fcntl.h>
-//#include <unistd.h>
-//#include <sys/mman.h>
+
+#include <unistd.h>
+#include <sys/mman.h>
 #include <sys/shm.h>
 #include <sys/stat.h>
+#include <fcntl.h>
 #endif
 
-//#include <stdexcept>
-//#include <streambuf>
-//#include <cmath>
 
 #if defined(_WIN32) | defined(__WIN32__) | defined(__WIN32) | defined(_WIN64) | defined(__WIN64)
-#define MAP_FAILED NULL
+const int MAP_FAILED = NULL;
 #endif
 
 
@@ -30,75 +28,41 @@ class mem_map
 {
 public:
             
-    mem_map(std::string name_, uint64_t data_size) : name(name_)
+    mem_map(std::string name_, uint64_t ds_) : name(name_), data_size(ds_)
     {
-    //#if defined(_WIN32) | defined(__WIN32__) | defined(__WIN32) | defined(_WIN64) | defined(__WIN64)          
-    //    map_handle_ = NULL;
-    //#else
-    //    file_handle_ = -1;
-    //#endif
 
-    //    map_address_ = MAP_FAILED;
                 
     #if defined(_WIN32) | defined(__WIN32__) | defined(__WIN32) | defined(_WIN64) | defined(__WIN64)
-        //file_handle_ = ::CreateFile(
-        //    file_name.c_str(),
-        //    GENERIC_READ | GENERIC_WRITE,
-        //    FILE_SHARE_READ | FILE_SHARE_WRITE,
-        //    NULL,
-        //    OPEN_ALWAYS,
-        //    FILE_ATTRIBUTE_NORMAL,
-        //    NULL);
 
-        //if (file_handle_ == INVALID_HANDLE_VALUE) 
-        //{
-        //    std::cout << "Error opening file: " << file_name << " " << GetLastError() << std::endl;
-        //    return;
-        //}
+        map_handle = ::CreateFileMapping(INVALID_HANDLE_VALUE, NULL, PAGE_READWRITE, 0, data_size, name.c_str());
 
-        //map_handle_ = ::CreateFileMapping(file_handle_, NULL, PAGE_READWRITE, 0, 0, NULL);
-        map_handle_ = ::CreateFileMapping(INVALID_HANDLE_VALUE, NULL, PAGE_READWRITE, 0, data_size, name.c_str());
-
-        if (map_handle_ == NULL) 
+        if (map_handle == NULL) 
         {                    
             std::cout << "Error mapping file: " << GetLastError() << std::endl;
             close();
             return;
         }
 
-        map_address_ = ::MapViewOfFile(map_handle_, FILE_MAP_ALL_ACCESS, 0, 0, data_size);
+        map_address = ::MapViewOfFile(map_handle, FILE_MAP_ALL_ACCESS, 0, 0, data_size);
                 
     #else
-//        file_handle_ = open(name.c_str(), O_RDWR | O_CREAT | O_TRUNC);
-//        if (file_handle_ == -1)
-//        {
-//            std::cout << "Error opening file." << std::endl;
-//        }
-//
-//        struct stat sb;
-//        if (fstat(file_handle_, &sb) == -1)
-//        {
-//            std::cout << "Error mapping file." << std::endl;
-//            close();
-//}
-//        file_size_ = sb.st_size;
 
-        map_handle_ = shm_open(name.c_str(), O_RDWR | O_CREAT, S_IRUSR | S_IWUSR);
-        if (map_handle_ == -1)
+        map_handle = shm_open(name.c_str(), O_RDWR | O_CREAT, S_IRUSR | S_IWUSR);
+        if (map_handle == -1)
         {
             std::cout << "Error opening shared memory..." << std::endl;
         }
 
-        if (ftruncate(map_handle_, data_size) == -1)
+        if (ftruncate(map_handle, data_size) == -1)
         {
             std::cout << "Error configuring the size of the shared memory object..." << std::endl;
         }
 
-        map_address_ = mmap(NULL, data_size, PROT_READ | PROT_WRITE, MAP_SHARED, map_handle_, 0);
+        map_address = mmap(NULL, data_size, PROT_READ | PROT_WRITE, MAP_SHARED, map_handle, 0);
 
     #endif
 
-        if (map_address_ == MAP_FAILED) 
+        if (map_address == MAP_FAILED) 
         {
             std::cout << "Error getting map address..." << std::endl;
             close();
@@ -110,38 +74,26 @@ public:
     void close()
     {
     #if defined(_WIN32) | defined(__WIN32__) | defined(__WIN32) | defined(_WIN64) | defined(__WIN64)
-        if (map_address_) 
+        if (map_address != MAP_FAILED) 
         {
-            ::UnmapViewOfFile(map_address_);
-            map_address_ = MAP_FAILED;
+            ::UnmapViewOfFile(map_address);
+            map_address = MAP_FAILED;
         }
 
-        if (map_handle_) 
+        if (map_handle) 
         {
-            ::CloseHandle(map_handle_);
-            map_handle_ = NULL;
+            ::CloseHandle(map_handle);
+            map_handle = NULL;
         }
-
-        //if (file_handle_ != INVALID_HANDLE_VALUE) 
-        //{
-        //    ::CloseHandle(file_handle_);
-        //    file_handle_ = INVALID_HANDLE_VALUE;
-        //}
             
     #else
-        if (map_address_ != MAP_FAILED) 
+        if (map_address != MAP_FAILED) 
         {
-            munmap(map_address_, size_);
-            map_address_ = NULL;
+            munmap(map_address, data_size);
+            map_address = NULL;
         }
 
-        //if (file_handle_ != -1) 
-        //{
-        //    close(file_handle_);
-        //    file_handle_ = -1;
-        //}
-
-        close(file_handle_);
+        //close();
 
         // remove the shared memory object
         shm_unlink(name.c_str());
@@ -152,26 +104,33 @@ public:
         
 // ----------------------------------------------------------------------------
     template <typename T>
-    inline void read(uint64_t &position, T &data)
+    inline T* get_address(uint64_t position)
+    {
+        return (reinterpret_cast<T*>(map_address) + position);
+    }
+
+// ----------------------------------------------------------------------------
+    template <typename T>
+    inline void read(uint64_t position, T &data)
     {
         uint8_t* bytes = reinterpret_cast<uint8_t*>(&data);
 
         for (uint8_t idx = 0; idx < sizeof(T); ++idx)
         {
-            read_data(position, bytes[idx]);
+            read_data(position + idx, bytes[idx]);
         }
     }
 
 // ----------------------------------------------------------------------------
     template <typename T>
-    inline void read_range(uint64_t &position, uint64_t length, std::vector<T> &data)
+    inline void read_range(uint64_t position, uint64_t length, std::vector<T> &data)
     {
         data.clear();
             
         for(uint64_t idx=0; idx<length; ++idx)
         {
             T d;
-            read(position, d);
+            read(position + idx, d);
             data.push_back(d);       
         }         
     }   // end of read_range
@@ -184,7 +143,7 @@ public:
 
         for (uint8_t idx = 0; idx < sizeof(T); ++idx)
         {
-            write_data(position+idx, bytes[idx]);
+            write_data(position + idx, bytes[idx]);
         }
     }   // end of write
 
@@ -197,7 +156,7 @@ public:
 
         for (uint64_t idx = 0; idx < data.size(); ++idx)
         {
-            write(position+idx, data[idx]);
+            write(position + idx, data[idx]);
         }
     }   // end of write_range
 
@@ -218,27 +177,27 @@ public:
 private:
         
 #if defined(_WIN32) | defined(__WIN32__) | defined(__WIN32) | defined(_WIN64) | defined(__WIN64)
-    HANDLE map_handle_;
+    HANDLE map_handle;
 #else
-    int32_t map_handle_;
+    int32_t map_handle;
 #endif
         
-    void* map_address_;
+    void* map_address;
 
     std::string name;
+    
+    uint64_t data_size;
 
 // ----------------------------------------------------------------------------           
-    inline void read_data(uint64_t& position, uint8_t& data)
+    inline void read_data(uint64_t position, uint8_t& data)
     {
-        data = *((uint8_t*)(map_address_)+position);
-        ++position;
+        data = *((uint8_t*)(map_address)+position);
     }   // end of read_data
             
     inline void write_data(uint64_t position, uint8_t data)
     {
-        uint8_t* d = ((uint8_t*)(map_address_)+position);
+        uint8_t* d = ((uint8_t*)(map_address)+position);
         *d = data;
-        ++position;
     }   // end of write_data
 
 
