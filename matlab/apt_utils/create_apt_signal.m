8b8c2f0c157565dc1b61cc4255515a649dd0a112
davemers0160 <davemers@iu.edu>
2022-01-18 08:09:56 -0500

    updates to APT generation files

--- matlab/apt_utils/create_apt_signal.m
+++ matlab/apt_utils/create_apt_signal.m
@@ -38,23 +38,44 @@ sig_idx = 0;
 
 buffer = []; %zeros(OVERSAMPLE*numel(SYNCA));
 
-for idx=1:numel(SYNCA)
-    [buf, sig_idx] = modulate_signal(255*SYNCA(idx), sig_idx, CARRIER, OVERSAMPLE, BAUD);
-    buffer = cat(1, buffer, buf);
 
+img = meshgrid(floor(0:0.25:255.75));
+
+
+
+for idx=1:size(img,1)
+
+
+    buf_a = generate_apt_line(SYNCA, idx, img);
+    buf_b = generate_apt_line(SYNCB, idx, img);
+    
+    [buf_a, sig_idx] = modulate_signal(buf_a, sig_idx, CARRIER, OVERSAMPLE, BAUD);
+    [buf_b, sig_idx] = modulate_signal(buf_b, sig_idx, CARRIER, OVERSAMPLE, BAUD);
+
+    buffer = cat(1, buffer, buf_a, buf_b);
+
+    bp = 1;
 end
 
-for idx=1:47
-    [buf, sig_idx] = modulate_signal(0, sig_idx, CARRIER, OVERSAMPLE, BAUD);
-    buffer = cat(1, buffer, buf);
 
-end
 
-for idx=1:256
-    [buf, sig_idx] = modulate_signal(idx-1, sig_idx, CARRIER, OVERSAMPLE, BAUD);
-    buffer = cat(1, buffer, buf);
-
-end
+% for idx=1:numel(SYNCA)
+%     [buf, sig_idx] = modulate_signal(255*SYNCA(idx), sig_idx, CARRIER, OVERSAMPLE, BAUD);
+%     buffer = cat(1, buffer, buf);
+% 
+% end
+% 
+% for idx=1:47
+%     [buf, sig_idx] = modulate_signal(0, sig_idx, CARRIER, OVERSAMPLE, BAUD);
+%     buffer = cat(1, buffer, buf);
+% 
+% end
+% 
+% for idx=1:256
+%     [buf, sig_idx] = modulate_signal(idx-1, sig_idx, CARRIER, OVERSAMPLE, BAUD);
+%     buffer = cat(1, buffer, buf);
+% 
+% end
 
 %% modulate signal
 
@@ -67,27 +88,29 @@ end
 
 %% extra functions
 
-function [buffer, sig_idx] = modulate_signal(value, sig_idx, CARRIER, OVERSAMPLE, BAUD) 
+function [buffer, sig_idx] = modulate_signal(data, sig_idx, CARRIER, OVERSAMPLE, BAUD) 
 
-    buffer = zeros(OVERSAMPLE,1);
+    buffer = zeros(OVERSAMPLE*numel(data),1);
     
-    for idx=1:OVERSAMPLE
-        
-        samp = sin(CARRIER * 2.0 * pi() * (sig_idx / (BAUD * OVERSAMPLE)));
-        
-        %map_value(F value, F f1, F t1, T f2, T t2) 
-        %return f2 + ((t2 - f2) * (value - f1)) / (t1 - f1);
-        samp = samp * (0.0 + ((0.8 - 0.0) * (value - 0.0)) / (255 - 0.0));   %map_value((int)value, 0(f1), 255(t1), 0.0(f2), 0.7(t2));
+    for jdx=1:numel(data)
+    
+        for idx=1:OVERSAMPLE
 
-%         uint8_t buf = map_value(samp, -1.0, 1.0, 0, 255);
-%         buffer.push_back(buf);
-        
-        buffer(idx) = floor((0 + ((255 - 0.0) * (samp +1)) / (1+1)));
+            carr = sin(CARRIER * 2.0 * pi() * (sig_idx / (BAUD * OVERSAMPLE)));
+            value = (0.0 + ((0.8 - 0.0) * (data(jdx) - 0.0)) / (255 - 0.0));
 
-        sig_idx = sig_idx + 1;
+            %map_value(F value, F f1, F t1, T f2, T t2) 
+            %return f2 + ((t2 - f2) * (value - f1)) / (t1 - f1);
+            samp = carr * value;    %map_value((int)value, 0(f1), 255(t1), 0.0(f2), 0.7(t2));
+
+    %         uint8_t buf = map_value(samp, -1.0, 1.0, 0, 255);
+    %         buffer.push_back(buf);
+
+            buffer(idx) = floor((0 + ((255 - 0.0) * (samp +1)) / (1+1)));
+
+            sig_idx = sig_idx + 1;
+        end
     end
-
-    
     
 end 
 
--- matlab/apt_utils/generate_apt_line.m
+++ matlab/apt_utils/generate_apt_line.m
@@ -4,8 +4,10 @@ function data_line = generate_apt_line(sync_bits, line_num, img)
     data_line = [];
     frame_line = mod(line_num, 128);
     
+    [img_h, img_w, ~] = size(img);
+    
     % Sync
-    for idx=1:numel(sync_data)
+    for idx=1:numel(sync_bits)
         data_line(end+1) = 255*sync_bits(idx);
     end
     
@@ -15,12 +17,12 @@ function data_line = generate_apt_line(sync_bits, line_num, img)
     end
 
     % Image
-    if (line_num <= size(img,1))
-        for idx=1:size(img, 2)
+    if (line_num <= img_h)
+        for idx=1:img_w
             data_line(end+1) = img(line_num, idx);
         end
     else
-        for idx=1:size(img, 2)
+        for idx=1:img_w
             data_line(end+1) = 0;
         end
     end
