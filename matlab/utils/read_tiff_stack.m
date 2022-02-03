dc0a77d54852260cb02683c7afd3719c868b8938
davemers0160 <davemers@iu.edu>
2022-02-03 18:12:22 -0500

    adding a tiff stack viewer

--- matlab/utils/read_tiff_stack.m
+++ matlab/utils/read_tiff_stack.m
@@ -1,12 +1,26 @@
-function [img] = read_tiff_stack(filename)
+function [img] = read_tiff_stack(filename, varargin)
 
     info = imfinfo(filename);
     num_images = numel(info);
     img = cell(num_images,1);
     
-    for idx = 1:num_images
-        img{idx,1} = imread(filename, idx, 'Info', info);
+    norm_img = false;
+    
+    if(nargin==2)
+        norm_img = varargin{1};
     end
+    
+    if(norm_img == true)
+        for idx = 1:num_images
+            tmp = double(imread(filename, idx, 'Info', info));
+            img{idx,1} = floor( 255*(tmp - min(tmp(:))) / (max(tmp(:)) - min(tmp(:))) );
+        end
+    else
+        for idx = 1:num_images
+            img{idx,1} = double(imread(filename, idx, 'Info', info));
+        end
+    end
+    
 
 
 end
