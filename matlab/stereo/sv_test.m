95de923fe0dbe67df6b7e55a654e724fe98a8df6
davemers0160 <davemers@iu.edu>
2022-02-14 14:22:37 -0500

    adding error plot

--- matlab/stereo/sv_test.m
+++ matlab/stereo/sv_test.m
@@ -11,40 +11,47 @@ plot_num = 1;
 
 commandwindow;
 
-%% 
+%% setup a bunch of the parameters to describe the problem
+
+% number of measurements to try
+num = 1000;
+
+% min and max distances
+z_min = 500;
+z_max = 1100;
 
 % distance from camera to object (m)
-z = 100:100:1000;
-z = 1900*rand(1,20) + 100;
+z = 10*randi([0 60], num, 1) + z_min;
 
 % focal length (m)
-f = 1.45;
+f = .45;
 
 % camera baseline (distance between cameras) (m)
-b = 0.3;
+b = 0.5;
 
 % lens aperature (m)
 a = 0.095;
 
 % pixel size
-px_size = 3.45e-6;
+px_size = (3.45e-6)/2;
 
 % max angle for each lens
 max_a = atan2(a/2, f);
 
 % location of point on x-axis (m)
-% x = 0.0*b;
-x = 100*(2*rand(1,20)-1);
-
+% r = a + (b-a).*rand(N,1).
+x = zeros(num, 1);
+for idx=1:num
+    x(idx) = (z(idx) * tan(-max_a) + b) + ((z(idx)*tan(max_a)) - (z(idx)*tan(-max_a) + b))*(rand(1));
+end
 
 %%
 
 % calculate angles
 theta_l = atan2(x, z);
-
 theta_r = atan2(x-b, z);
 
-
+% calculate the disparity parts
 x_l = f*tan(theta_l);
 x_l = ceil(x_l/px_size) * px_size;
 
@@ -81,10 +88,44 @@ scatter(x, z, 20, 'ok', 'filled');
 
 scatter(x, Z, 10, 'og', 'filled');
 
-plot([0, -1000*tan(max_a)], [0, 1000], 'k')
-plot([0, 1000*tan(max_a)], [0, 1000], 'k')
+plot([0, -z_max*tan(max_a)], [0, z_max], 'k')
+plot([0, z_max*tan(max_a)], [0, z_max], 'k')
 
-plot([b, b-1000*tan(max_a)], [0, 1000], 'k')
-plot([b, b+1000*tan(max_a)], [0, 1000], 'k')
+plot([b, b-z_max*tan(max_a)], [0, z_max], 'k')
+plot([b, b+z_max*tan(max_a)], [0, z_max], 'k')
+
+%% create an error map with 1 meter increments in x and 5 meter increments in z
+
+x_range = floor(z_max*tan(-max_a) + b):0.5:ceil(z_max*tan(max_a));
+z_range = z_min:0.5:z_max;
+
+[x_g, z_g] = meshgrid(x_range, z_range);
+
+% calculate angles
+theta_lg = atan2(x_g, z_g);
+theta_rg = atan2(x_g-b, z_g);
+
+% calculate the disparity parts
+x_l = f*tan(theta_lg);
+x_lq = ceil(x_l/px_size) * px_size;
+
+x_r = f * tan(theta_rg);
+x_rq = ceil(x_r/px_size) * px_size;
+
+% calculate the quantized distance
+d_q = x_lq - x_rq;
+
+Z_q = f * b ./ d_q;
+
+
+% calculate the error (L1)
+Z_err = abs(z_g - Z_q);
+
+figure;
+surf(x_range, z_range, Z_err)
+shading flat
+colormap(jet(100));
+colorbar;
+view(90,90);
 
 
