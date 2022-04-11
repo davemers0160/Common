76642176adf06daa21e3bb89c542cbe0066d5397
davemers0160 <davemers@iu.edu>
2022-04-11 11:17:59 -0400

    Update aoa_2d_test.m

--- matlab/aoa/aoa_2d_test.m
+++ matlab/aoa/aoa_2d_test.m
@@ -1,3 +1,14 @@
+%% Angle of Arrival Calculations
+% Assumptions:
+% 1. Everything is 3-D but if we assume that the Z component is at 0 height
+% we can project everything to a 2-D plane
+% 2. We are working in a Cartessian Coordinate System (CCS) (Local Tangent Plane,
+% ECEF, ect...)
+% 3. The conversion from the CCS to a Polar Coordinate System and back is
+% well defined
+% 4. Solution uses Least Squares to solve and assumes we have all of the
+% data.  A Recursive Least Squares solution can be used as well.
+
 format long g
 format compact
 clc
@@ -10,8 +21,6 @@ full_path = mfilename('fullpath');
 plot_num = 1;
 
 %% set up each of the points
-%rng(now);
-
 
 % estimating +/- position error (kilometers)
 position_err = 0.005;
@@ -20,14 +29,12 @@ position_err = 0.005;
 angle_err = 1.3;
 
 % set the receiver locations (P) and the emitter location (T)
- 
 % 2D
-% receiver measurement locations
-P(:,1) = linspace(-60, -20, 51);
-P(:,2) = linspace(50, 15, 51);
+% receiver measurement locations (km)
+P(:,1) = linspace(-60, -30, 31);
+P(:,2) = linspace(35, 0, 31);
 
-
-% target location
+% target location (km)
 T = [10, 180];
 
 % get the dimensions of the data
@@ -39,14 +46,12 @@ for idx=1:N
     AoA(idx) = atan2d(T(2)-P(idx,2), T(1)-P(idx,1));
 end
 
+%% setup the measurement collection with errors
 % set the number of trials
 num_trials = 100;
 
 T_est = zeros(num_trials, D);
-iter = zeros(num_trials, 1);
-err = zeros(num_trials, 1);
 P_n = zeros(N, D, num_trials);
-
 
 for idx=1:num_trials
     % introduce random sensor and position noise
@@ -59,13 +64,13 @@ for idx=1:num_trials
     % build G
     G = cat(2, sind(AoA_n), -cosd(AoA_n));
     
-    % invert G -> ((GtG)^-1)Gt
+    % invert G: G^-1 ~= ((GtG)^-1)Gt, Gt = G transpose
     G_pinv = (pinv(G.' * G)*G.');
     
+    % x = G^-1 * b
     T_est(idx, :) = (G_pinv * b).';
     
 end
-
 
 %% get the covariance matrix, just do 2D for now
 Tn = T_est(:,1:2).';
@@ -88,17 +93,12 @@ Vp = Vp*s;
 [max_eig_val, max_eig_ind] = max(Vp);
 [min_eig_val, min_eig_ind] = min(Vp);
 
-max_eig_vec = Ep(:, max_eig_ind);
-min_eig_vec = Ep(:, min_eig_ind);
-
 % set the ellipse plotting segments
 theta = linspace(0, 2*pi, 100);
 
 % calculate the ellipse
 r_ellipse = (Ep * (sqrt(diag(Vp)))) * [cos(theta(:))'; sin(theta(:))'];
 
-% [V, D] = eig(Rp * s);
-% r_ellipse = (V * sqrt(D)) * [cos(theta(:))'; sin(theta(:))'];
 ecc = sqrt(1-(min(Vp)/max(Vp)));
 aou = prod(sqrt(Vp))*pi();
 
@@ -115,23 +115,20 @@ hold on
 grid on
 box on
 
-% for idx=1:N
-%     rectangle('Position',[P(idx,1)-position_err, P(idx,2)-position_err, 2*position_err, 2*position_err], 'FaceColor', [0.8 0.8 0.8 0.3], 'EdgeColor', [0 0 0], 'Curvature',[1,1]);  
-% end
-
+% plot the true measurement positions
 s1 = scatter(P(:,1), P(:,2), 20, 'v', 'filled', 'k');
-%scatter(Po(1), Po(2), 20, 'd', 'filled', 'b')
+% plot the estimated target locations
 s2 = scatter(T_est(:,1), T_est(:,2), '.', 'b');
 
-% plot the center
+% plot the center of the AoU
 s3 = scatter(C(1), C(2), 'o', 'filled', 'g');
+% plot the actual target location
 s4 = scatter(T(1), T(2), 'o', 'filled', 'r');
 
-% plot the ellipse
+% plot the AoU ellipse
 p1 = plot(r_ellipse(1,:) + C(1), r_ellipse(2,:) + C(2), '-g');
 
 set(gca,'fontweight','bold','FontSize',13);
-%xlim([floor((min(P(:,1))-1)/10)*10, ceil((max(P(:,1))+1)/10)*10]);
 xlabel('X (km)', 'fontweight','bold','FontSize',13);
 
 ytickformat('%1.1f')
