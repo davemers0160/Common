e42a5787f924e1d18da3875a1e880d5eb4e56d53
Dave <davemers@iu.edu>
2022-04-18 21:58:59 -0400

    getting color mapping working right

--- python/spectrum_viewer/spectrum_viewer.py
+++ python/spectrum_viewer/spectrum_viewer.py
@@ -147,36 +147,33 @@ def generate_spectrogram(iq_data, N, O, fs):
 
 
 def update_plot(attr, old, new):
-    global iq_data, spectrogram_data, color_bar, spectrogram_img
+    global iq_data, spectrogram_data
 
-    # print("update")
     spectrogram_data, freq, time = generate_spectrogram(iq_data, fft_length.value, fft_overlap.value, sample_rate.value*1e6)
-    # f, t, spectrogram_data = signal.spectrogram(iq_data, sample_rate.value, window=signal.windows.hamming(fft_length.value, sym=True),
-    #                                             nperseg=fft_length.value, noverlap=fft_overlap.value, return_onesided=False,
-    #                                             detrend=False, mode='complex',
-    #                                             scaling='spectrum')
-    # spectrogram_data = np.fft.fftshift(np.nan_to_num((20 * np.log10(np.abs(spectrogram_data*np.conj(spectrogram_data)))), neginf=-200, posinf=200).T, axes=1)
 
     freq = np.tile(freq, spectrogram_data.shape[0])
     time = np.repeat(time, spectrogram_data.shape[1])
 
     spectrogram_source.data = {'spectrogram_img': [spectrogram_data], 'x': [np.min(freq)], 'y': [0],
                                'dw': [sample_rate.value], 'dh': [np.max(time)], 't': [time], 'f': [freq]}
-    # spectrogram_source.data = {'spectrogram_img': [spectrogram_data]}
-    jet_mapper = LinearColorMapper(palette=jet_colormap(200), low=min_amp.value, high=max_amp.value)
-    color_bar = ColorBar(color_mapper=jet_mapper, label_standoff=6)
-
-    spectrogram_img.glyph.color_mapper.low = min_amp.value
-    # spectrogram_fig.image(image='spectrogram_img', x=-10, y=0, dw=20, dh=0.5, global_alpha=1.0, dilate=False, palette=jet_1k, source=spectrogram_source)
 
     bp = 1
+
+
+# -----------------------------------------------------------------------------
+def update_color_scale(attr, old, new):
+    global spectrogram_img
+
+    spectrogram_img.glyph.color_mapper.low = min_amp.value
+    spectrogram_img.glyph.color_mapper.high = max_amp.value
+
 
 # -----------------------------------------------------------------------------
 def get_input():
     global iq_filename, iq_data_path, iq_data, spectrogram_data, f, t
 
-    iq_filename = QFileDialog.getOpenFileName(None, "Select a file",  iq_data_path, "IQ files (*.bin *.dat);;All files (*.*)")
-    # iq_filename = ["D:/Projects/rf_zsl/data/sdr_test_10M_100m_0000.bin"]
+    # iq_filename = QFileDialog.getOpenFileName(None, "Select a file",  iq_data_path, "IQ files (*.bin *.dat);;All files (*.*)")
+    iq_filename = ["D:/Projects/rf_zsl/data/sdr_test_10M_100m_0000.bin"]
 
     filename_div.text = "File name: " + iq_filename[0]
     if(iq_filename[0] == ""):
@@ -200,6 +197,7 @@ def get_input():
     update_plot(1, 1, 1)
 
 
+# -----------------------------------------------------------------------------
 # the main entry point into the code
 file_select_btn = Button(label='Select File', width=100)
 file_select_btn.on_click(get_input)
@@ -219,14 +217,9 @@ spectrogram_fig = figure(plot_height=800, plot_width=1300, title="Spectrogram",
                          tools="save, pan, box_zoom, reset, wheel_zoom, hover, crosshair", active_drag="box_zoom",
                          active_scroll="wheel_zoom", active_inspect=None)
 
-# spectrogram_fig.add_tools(hover)
-
-
-# spectrogram_fig.image(image='spectrogram_img', x=-5, y=0, dw=10, dh=0.1, global_alpha=1.0, dilate=False, palette=jet_1k, source=spectrogram_source)
 spectrogram_img = spectrogram_fig.image(image='spectrogram_img', x='x', y='y', dw='dw', dh='dh', global_alpha=1.0, dilate=False, color_mapper=jet_mapper, source=spectrogram_source)   # palette=jet_1k,
 
 spectrogram_fig.add_layout(color_bar, 'right')
-
 spectrogram_fig.x_range.range_padding = 0
 spectrogram_fig.y_range.range_padding = 0
 
@@ -247,9 +240,11 @@ spectrogram_fig.yaxis.axis_label_text_font_style = "bold"
 spectrogram_fig.yaxis.axis_label = "Time (s)"
 
 # setup the event callbacks for the plot
-for w in [fft_length, fft_overlap, sample_rate, max_amp, min_amp]:
+for w in [fft_length, fft_overlap, sample_rate]:
     w.on_change('value', update_plot)
 
+for w in [max_amp, min_amp]:
+    w.on_change('value', update_color_scale)
 
 # create the layout for the controls
 btn_layout = row(file_select_btn, Spacer(width=15), filename_div)
