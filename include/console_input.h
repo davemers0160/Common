8b0a0ea368da7266db50d0b02fdfac0c9a9c386b
davemers0160 <davemers@iu.edu>
2022-05-31 15:47:29 -0400

    Update console_input.h

--- include/console_input.h
+++ include/console_input.h
@@ -19,11 +19,16 @@ class console_input
 {
 public:
 	
-    bool running = true;
+    console_input()
+    {
+        valid_input = false;
+    }
 
-	console_input() = default;
 
-	//console_input(bool r_) : running(r_) {}
+    //-----------------------------------------------------------------------------
+    //void set_running(bool v) { running = v; }
+
+    bool get_running() { return running; }
 
 
     //-----------------------------------------------------------------------------
@@ -34,60 +39,80 @@ public:
         valid_input = false;
     }
 
-    //-----------------------------------------------------------------------------
-    //void run_t(void)
-    //{
-    //    std::thread run_thread(&console_input::run, this);
-
-    //    run_thread.join();
-    //}
+    unsigned char get_input(void) { return ch2; }
 
     //-----------------------------------------------------------------------------
-    inline void run(void)
+    void arrow_key(void)
+    {
+        running = true;
+        valid_input = false; 
+        run_thread = std::thread(&console_input::run_arrow_key, this);
+    }
+
+    //-----------------------------------------------------------------------------
+    inline void run_arrow_key(void)
     {
         while (running)
         {
+            std::this_thread::sleep_for(std::chrono::milliseconds(20));
+     
             unsigned char ch1 = _getch();
             if ((ch1 == KEY_ARROW_CHAR1) || (ch1 == 0))
             {
                 // Some Arrow key was pressed, determine which?
-                unsigned char ch2 = _getch();
-                switch (ch2)
-                {
-                case KEY_ARROW_UP:
-                    // code for arrow up
-                    cout << "KEY_ARROW_UP" << endl;
-                    break;
-                case KEY_ARROW_DOWN:
-                    // code for arrow down
-                    cout << "KEY_ARROW_DOWN" << endl;
-                    break;
-                case KEY_ARROW_LEFT:
-                    // code for arrow right
-                    cout << "KEY_ARROW_LEFT" << endl;
-                    break;
-                case KEY_ARROW_RIGHT:
-                    // code for arrow left
-                    cout << "KEY_ARROW_RIGHT" << endl;
-                    break;
-                }
+                ch2 = _getch();
+                //switch (ch2)
+                //{
+                //case KEY_ARROW_UP:
+                //    // code for arrow up
+                //    std::cout << "KEY_ARROW_UP" << std::endl;
+                //    break;
+                //case KEY_ARROW_DOWN:
+                //    // code for arrow down
+                //    std::cout << "KEY_ARROW_DOWN" << std::endl;
+                //    break;
+                //case KEY_ARROW_LEFT:
+                //    // code for arrow right
+                //    std::cout << "KEY_ARROW_LEFT" << std::endl;
+                //    break;
+                //case KEY_ARROW_RIGHT:
+                //    // code for arrow left
+                //    std::cout << "KEY_ARROW_RIGHT" << std::endl;
+                //    break;
+                //}
 
+                valid_input = true;
+            }
+            else if (ch1 == 27)
+            {
+                running = false;
                 valid_input = false;
             }
         }
+
+        run_thread.join();
     }   // end of run
 
+    void stop(void)
+    {
+        running = false;
+        run_thread.join();
+    }
 
 //-----------------------------------------------------------------------------
 private:
     static const int KEY_ARROW_CHAR1 = 224;
-    static const int KEY_ARROW_UP = 72;
-    static const int KEY_ARROW_DOWN = 80;
-    static const int KEY_ARROW_LEFT = 75;
-    static const int KEY_ARROW_RIGHT = 77;
+    //static const int KEY_ARROW_UP = 72;
+    //static const int KEY_ARROW_DOWN = 80;
+    //static const int KEY_ARROW_LEFT = 75;
+    //static const int KEY_ARROW_RIGHT = 77;
 
 	bool valid_input;
+    bool running;
 
+    std::thread run_thread;
+
+    unsigned char ch2;
 
 };  // end of class
 
