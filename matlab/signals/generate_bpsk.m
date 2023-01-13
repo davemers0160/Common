99f7dcea9963c2bb254c7ada0abb71ca1fe49f9f
davemers0160 <davemers0160@gmail.com>
2023-01-13 11:17:03 -0500

    Update generate_bpsk.m

--- matlab/signals/generate_bpsk.m
+++ matlab/signals/generate_bpsk.m
@@ -3,5 +3,14 @@ function [iq] = generate_bpsk(data, amplitude, sample_rate, bit_length)
     num_bits = numel(data);
     samples_per_bit = floor(sample_rate*bit_length);
     
+    iq = [];
     
+    for idx=1:num_bits
+    
+        iq = cat(1, iq, amplitude*data(idx)*ones(samples_per_bit,1));
+
+    end
+    
+    iq = complex(iq, zeros(numel(iq),1));
+
 end
