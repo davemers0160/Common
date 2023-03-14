44ab3262d6d5e0e270155634293342e2f3dbfeb4
davemers0160 <davemers0160@gmail.com>
2023-03-14 16:13:21 -0400

    Update rf_propagation.m

--- matlab/rf_propagation.m
+++ matlab/rf_propagation.m
@@ -13,6 +13,10 @@ line_width = 1;
 ft2m = 0.3048001109472;
 m2nmi = 0.000539957;
 
+
+% clear tempdir
+% setenv('TMP','C:/temper');
+
 commandwindow;
 
 %% setup propagation model
@@ -26,7 +30,7 @@ pm_fsl = propagationModel("freespace");
 %% tx parameters
 latitude = 42.0;
 longitude = -68.0;
-tx_antenna_height = 40000*ft2m;          % meters
+tx_antenna_height = 2000*ft2m;          % meters
 tx_power = 1;                       % Watts
 tx_freq = 1000e6;                   % Hz
 
@@ -34,7 +38,7 @@ tx = txsite('Name','M', 'Latitude', latitude, 'Longitude', longitude, 'Transmitt
 
 %% rx parameters
 
-latitude = linspace(42.000, 43.5, 151);
+latitude = linspace(42.000, 44.0, 201);
 longitude = -68.0*ones(numel(latitude),1);
 rx_antenna_height = 145*ft2m;       % meters
 rx_sensitivity = -200;              % dBm
@@ -49,7 +53,7 @@ signal_strength = -130:5:-20;       % dBm
 max_range = 3e5;                    % meters
 rx_gain = 0;                        % dB
 
-% coverage(tx, "PropagationModel", pm, 'MaxRange',max_range, 'SignalStrengths',signal_strength, 'ReceiverGain',rx_gain, 'Transparency',transparency, 'ReceiverAntennaHeight',rx_height);
+coverage(tx, "PropagationModel", pm_fsl, 'MaxRange',max_range, 'SignalStrengths',signal_strength, 'ReceiverGain',rx_gain, 'Transparency',transparency, 'ReceiverAntennaHeight',rx_height);
 
 %% try pathloss
 d = distance(tx, rx);
@@ -58,26 +62,36 @@ pl_fsl = pathloss(pm_fsl, rx, tx);
 % pl_lr = pathloss(pm_lr, rx, tx);
 
 %% plot
+
+x_min = 0;
+x_max = 120;
+y_min = -165;
+y_max = -15;
+
 figure(plot_num)
-set(gcf,'position',([50,50,1400,500]),'color','w')
+set(gcf,'position',([50,50,1400,700]),'color','w')
 plot(m2nmi*d, -pl_fsl, '.-b', 'LineWidth', line_width);
 box on
 grid on
 hold on
-% plot(m2nmi*d, 93-pl_lr, '.-g', 'LineWidth', line_width);
+% plot(m2nmi*d, -pl_lr, '.-g', 'LineWidth', line_width);
+plot([x_min,x_max], [-120, -120], '--g', 'LineWidth', line_width);
 
 set(gca,'fontweight','bold','FontSize', 13);
 
-xlim([0,90])
-xticks(0:5:90)
-% ylim([-150, -50])
+xlim([x_min,x_max])
+xticks(x_min:5:x_max)
+ylim([y_min, y_max])
+yticks([y_min:5:y_max])
 
-xlabel('Distance (nmi)', 'fontweight', 'bold', 'FontSize', 13);
+xlabel('Slant Range (nmi)', 'fontweight', 'bold', 'FontSize', 13);
 ylabel('Pathloss (dB)', 'fontweight', 'bold', 'FontSize', 13);
 
 title(strcat('Propagation Results: Tx Height =',32,num2str(floor(tx.AntennaHeight/ft2m+0.5), '%5d ft;'),32, 'Rx Height =',32,num2str(floor(rx(1).AntennaHeight/ft2m+0.5), '%5d ft')), 'fontweight', 'bold', 'FontSize', 14);
 
+legend('PathLoss','Min RSL');
+
 ax = gca;
-ax.Position = [0.06, 0.11, 0.9, 0.80];
+ax.Position = [0.06, 0.085, 0.9, 0.85];
 
 plot_num = plot_num + 1; 
