7febd0d008cbf8a273180221e5330eb4700ad8e3
dre_beast <davemers0160@gmail.com>
2023-06-04 14:03:30 -0400

    Update display_binary_iq.m

--- matlab/iq_utils/display_binary_iq.m
+++ matlab/iq_utils/display_binary_iq.m
@@ -38,8 +38,8 @@ fclose(file_id);
 %% display the FFT of the data in a spectrogram
 
 %sample rate
-fs =  10e6;
-fft_size = 4096;
+fs =  40e6;
+fft_size = 8192;
 
 
 fft_time_step = fft_size/fs;
@@ -55,48 +55,52 @@ fft_offset = 0;
 
 % y(num_ffts, :) = fftshift(fft(data(1+fft_offset:fft_size+fft_offset), fft_size))/fft_size;
 % fft_offset = fft_offset + fft_size;
+% 
+% for idx=1:num_ffts
+%     Z(idx, :) = fftshift(fft(data(1+fft_offset:fft_size+fft_offset), fft_size))/fft_size;
+%     fft_offset = fft_offset + fft_size;
+% end
+% 
+% % Z(1, :) = fftshift(fft(data(1+fft_offset:end), fft_size))/fft_size;
+% 
+% %% plot the fft
+% 
+% X = -fs/2:freq_bin:fs/2-freq_bin;
+% Y = 0:fft_time_step:num_ffts*fft_time_step-fft_time_step;
+% 
+% figure(plot_num)
+% set(gcf,'position',([100,100,1000,800]),'color','w')
+% 
+% surf(X, Y, 20*log10(abs(Z)));
+% %surf(X, Y, 2*abs(y));
+% shading interp;
+% colormap(jet(1024));
+% 
+% set(gca, 'fontweight', 'bold', 'FontSize', 13);
+% 
+% % X-Axis
+% xlim([X(1) X(end)]);
+% xticks(linspace(X(1), X(end), 11));
+% xticklabels([(-fs/2):1e6:fs/2]/1e6);
+% xlabel('Frequency (MHz)', 'fontweight', 'bold', 'FontSize', 13);
+% 
+% % Y-Axis
+% ylim([Y(1) Y(end)]);
+% yticks(linspace(Y(1), Y(end), 11));
+% %yticklabels([0:0.005:0.1]);
+% ytickformat('%1.3f')
+% ylabel('Time (s)', 'fontweight', 'bold', 'FontSize', 13);
+% 
+% view(0, 90);
+% plot_num = plot_num + 1;
+% 
+% %% 
+% figure(plot_num)
+% set(gcf,'position',([100,100,1000,800]),'color','w')
+% pwelch(data,[],[],[],fs,'centered');
+% plot_num = plot_num + 1;
 
-for idx=1:num_ffts
-    Z(idx, :) = fftshift(fft(data(1+fft_offset:fft_size+fft_offset), fft_size))/fft_size;
-    fft_offset = fft_offset + fft_size;
-end
+%%
 
-% Z(1, :) = fftshift(fft(data(1+fft_offset:end), fft_size))/fft_size;
-
-%% plot the fft
-
-X = -fs/2:freq_bin:fs/2-freq_bin;
-Y = 0:fft_time_step:num_ffts*fft_time_step-fft_time_step;
-
-figure(plot_num)
-set(gcf,'position',([100,100,1000,800]),'color','w')
-
-surf(X, Y, 20*log10(abs(Z)));
-%surf(X, Y, 2*abs(y));
-shading interp;
-colormap(jet(1024));
-
-set(gca, 'fontweight', 'bold', 'FontSize', 13);
-
-% X-Axis
-xlim([X(1) X(end)]);
-xticks(linspace(X(1), X(end), 11));
-xticklabels([(-fs/2):1e6:fs/2]/1e6);
-xlabel('Frequency (MHz)', 'fontweight', 'bold', 'FontSize', 13);
-
-% Y-Axis
-ylim([Y(1) Y(end)]);
-yticks(linspace(Y(1), Y(end), 11));
-%yticklabels([0:0.005:0.1]);
-ytickformat('%1.3f')
-ylabel('Time (s)', 'fontweight', 'bold', 'FontSize', 13);
-
-view(0, 90);
-plot_num = plot_num + 1;
-
-%% 
-figure(plot_num)
-set(gcf,'position',([100,100,1000,800]),'color','w')
-pwelch(data,[],[],[],fs,'centered');
-plot_num = plot_num + 1;
+spectrogram(data(1:40e6), 8192,4096,8192,fs,'centered');
 
