c91c6835c1ec160ef453ce65c583bcad2a5a1080
dre_beast <davemers0160@gmail.com>
2023-06-04 14:10:07 -0400

    Update read_binary_iq_data.m

--- matlab/iq_utils/read_binary_iq_data.m
+++ matlab/iq_utils/read_binary_iq_data.m
@@ -1,4 +1,4 @@
-function [iq, iqc, i_data, q_data] = read_binary_iq_data(filename, data_type, byte_order)
+function [iq, iqc] = read_binary_iq_data(filename, data_type, byte_order)
 
         % only supports the following data types: 'double', 'single',
         % u/int8, u/int16, u/int32, u/int64, float32, float64
@@ -34,8 +34,8 @@ function [iq, iqc, i_data, q_data] = read_binary_iq_data(filename, data_type, by
         iq = fread(file_id, [2, file_size], data_type, byte_order).';
         
         % separate into individual channels if needed
-        i_data = iq(:,1);
-        q_data = iq(:,2);
+        % i_data = iq(:,1);
+        % q_data = iq(:,2);
         
         iqc = complex(iq(:,1), iq(:,2));
 
