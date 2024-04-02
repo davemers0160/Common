4b86aacbefed66de2b3dad019d142059038053b5
Dave <davemers0160@gmail.com>
2024-04-02 18:41:42 -0400

    adding  oqpsk

--- matlab/signals/generate_oqpsk.m
+++ matlab/signals/generate_oqpsk.m
@@ -1,6 +1,6 @@
-function [iq] = generate_qpsk(data, sample_rate, bit_length)
+function [iq] = generate_oqpsk(data, sample_rate, half_bit_length)
 
-    samples_per_bit = floor(sample_rate * bit_length);
+    samples_per_bit = floor(sample_rate * half_bit_length);
 
     % check for odd number and append a 0 at the end if it is odd
     data = data(:);
@@ -13,14 +13,14 @@ function [iq] = generate_qpsk(data, sample_rate, bit_length)
     num_bit_pairs = size(d2, 1);
     
     % this will expand the bit to fill the right number of samples
-    s = ones(floor(samples_per_bit), 1);
+    s = ones(floor(2 * samples_per_bit), 1);
     
     % pre calculate the base 45 degree value
     v = sqrt(2)/2;
           
     % start with I and Q offset by half a bit length
     I = [];
-    Q = [];
+    Q = zeros(samples_per_bit,1);
     
     for idx=1:num_bit_pairs
     
@@ -47,7 +47,10 @@ function [iq] = generate_qpsk(data, sample_rate, bit_length)
         Q = cat(1, Q, v_q*s);
         
     end
-
+    
+    % add half a bit length of zeros to the I channel
+    I = cat(1, I, zeros(samples_per_bit,1));
+    
     % merge the I and Q channels
     iq = I + 1j*Q;
 
