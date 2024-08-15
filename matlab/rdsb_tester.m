e90df85f2e60d6fb08c4c8bebd9d2cd9cb6587ff
david emerson <davemers0160@gmail.com>
2024-08-14 22:03:34 -0400

    Update rdsb_tester.m

--- matlab/rdsb_tester.m
+++ matlab/rdsb_tester.m
@@ -4,80 +4,120 @@ clc
 close all
 clearvars
 
+% get the location of the script file to save figures
 full_path = mfilename('fullpath');
 [startpath,  filename, ext] = fileparts(full_path);
 plot_num = 1;
+line_width = 1;
 
 commandwindow;
 
-%% RDSB Setup
+%%
+samples_per_symbol = 24;
+groups_per_frame = 100;
+program_identification_code = [0 1 1 1 0 0 1 0 1 1 0 0 0 0 0 0];
 
-userInput = helperRBDSInit();
-userInput.Duration = 40;
-% userInput.SignalSource = 'File';
-% userInput.SignalFilename = 'rbds_capture.bb';
-userInput.SignalSource = 'RTL-SDR';
-userInput.CenterFrequency = 97.7e6;
-% userInput.SignalSource = 'ADALM-PLUTO';
-% userInput.CenterFrequency = 98.5e6;
-% userInput.SignalSource = 'USRP';
-% userInput.CenterFrequency = 98.5e6;
-userInput.RadioAddress = '0';
+%%
+rbdsgen = comm.RBDSWaveformGenerator(SamplesPerSymbol=samples_per_symbol, GroupsPerFrame=groups_per_frame, RadioText='Test Radio!', ...
+    ProgramServiceName='ABABABAB', ProgramIdentificationCode=program_identification_code, ProgramType="Rock")
 
-[rbdsParam, sigSrc] = helperRBDSConfig(userInput)
+Y = step(rbdsgen).';
 
-%% 
 
-fmBroadcastDemod = comm.FMBroadcastDemodulator(...
-    'SampleRate',228e3, ...
-    'FrequencyDeviation',rbdsParam.FrequencyDeviation, ...
-    'FilterTimeConstant',rbdsParam.FilterTimeConstant, ...
-    'AudioSampleRate',rbdsParam.AudioSampleRate, ...
-    'Stereo',true);
+figure(plot_num);
+plot(Y, '.-b')
+plot_num = plot_num + 1;
 
-% Create audio player
-player = audioDeviceWriter('SampleRate',rbdsParam.AudioSampleRate);
+%% upsample
 
-% Layer 2 object
-datalinkDecoder = RBDSDataLinkDecoder();
+factor = 20;
 
-% Layer 3 object
-sessionDecoder  = RBDSSessionDecoder();
-% register processing implementation for RadioText Plus (RT+) ODA:
-rtID = '4BD7';
-registerODA( ...
-    sessionDecoder,rtID,@RadioTextPlusMainGroup,@RadioTextPlus3A);
+sample_rate = samples_per_symbol * factor * 1187.5;
 
-% Create the data viewer object
-viewer = helperRBDSViewer();
+rbds_up = upsample(Y, factor);
 
-% Start the viewer and initialize radio time
-start(viewer)
-radioTime = 0;
-% Main loop
-while radioTime < rbdsParam.Duration
-    % Receive baseband samples (Signal Source)
-    rcv = sigSrc();
+N = 8*factor + 1;
 
-    % Demodulate FM broadcast signals and play the decoded audio
-    audioSig = fmBroadcastDemod(rcv);
-    player(audioSig);
+w = blackman_nuttall_window(N);
 
-    % Process physical layer (Layer 1)
-    bitsPHY = RBDSPhyDecoder(rcv, rbdsParam);
+% create the full filter using the window
+lpf = N*create_fir_filter(2375/sample_rate, w);
 
-    % Process data-link layer (Layer 2)
-    [enabled,iw1,iw2,iw3,iw4] = datalinkDecoder(bitsPHY);
+% rolloff = 1;
+% no_of_symbols = 1;
+% lpf = rcosdesign(rolloff, no_of_symbols, (samples_per_symbol * factor) );
 
-    % Process session and presentation layer (Layer 3)
-    outStruct = sessionDecoder(enabled,iw1,iw2,iw3,iw4);
+% apply the filter to the bpsk signal
+rbds_conv = 160*conv(rbds_up, lpf(end:-1:1), 'same');
 
-    % View results packet contents (Data Viewer)
-    update(viewer, outStruct);
+figure(plot_num);
+plot(160*rbds_up, '-b')
+hold on
+box on
+grid on
+plot(rbds_conv, '-g')
+plot_num = plot_num + 1;
 
-    % Update radio time
-    radioTime = radioTime + rbdsParam.FrameDuration;
-end
 
-% Stop the viewer and release the signal source and audio writer
-stop(viewer);
+%%
+
+pilot_freq = 19000;
+
+rbds_freq = 57000;
+
+% pilot_tone = (800*exp(1i*2*pi()*(pilot_freq/sample_rate)*(0:1:numel(rbds_conv)-1)));
+% rbds_rot = exp(1i*2*pi()*(rbds_freq/sample_rate)*(0:1:numel(rbds_conv)-1));
+
+% rbds_conv_c = complex(rbds_conv);
+% iq_rbds = (rbds_conv_c .* rbds_rot);
+
+% iq_data = int16(pilot_tone + iq_rbds);
+
+pilot_tone = 1200*cos(2*pi()*(pilot_freq/sample_rate)*(0:1:numel(rbds_conv)-1));
+
+rbds_rot = 2*cos(2*pi()*(rbds_freq/sample_rate)*(0:1:numel(rbds_conv)-1));
+
+iq_rbds = (rbds_conv .* rbds_rot);
+iq_data = complex(int16(pilot_tone + iq_rbds));
+
+
+figure(plot_num);
+plot(real(iq_rbds), '-b')
+plot_num = plot_num + 1;
+
+max(real(iq_data))
+max(imag(iq_data))
+min(real(iq_data))
+min(imag(iq_data))
+
+%%
+fft_rbds = fft(double(iq_data)/2048)/numel(iq_data);
+
+x_rbds = linspace(-sample_rate/2, sample_rate/2, numel(fft_rbds));
+
+figure(plot_num)
+set(gcf,'position',([50,50,1400,500]),'color','w')
+hold on;
+plot(x_rbds/1e3, 20*log10(abs(fftshift(fft_rbds))),'k')
+% plot(x_rbds/1e6, 20*log10(abs(fftshift(fft_x1))),'g')
+grid on
+box on
+set(gca,'fontweight','bold','FontSize', 13);
+xlim([x_rbds(1), x_rbds(end)]/1e3);
+xlabel('Frequency (KHz)', 'fontweight', 'bold', 'FontSize', 13);
+ylabel('Amplitude', 'fontweight', 'bold', 'FontSize', 13);
+title('Filtered vs. Un-Filtered Signal', 'fontweight', 'bold', 'FontSize', 14);
+
+plot_num = plot_num + 1;
+
+%%
+data_type = 'int16';
+byte_order = 'ieee-le';
+
+filename = 'D:\Projects\data\RF\test_rds.sc16';
+
+write_binary_iq_data(filename, iq_data, data_type, byte_order);
+
+fprintf('complete\n');
+
+
