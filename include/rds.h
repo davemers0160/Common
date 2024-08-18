6ba9121628c8e486976f5fc7051d664547338bd5
Dave <davemers@iu.edu>
2024-08-17 23:28:57 -0400

    building the rds generator

--- include/rds.h
+++ include/rds.h
@@ -23,34 +23,50 @@
 #include <algorithm>
 //#include <random>
 
+#include <dsp/dsp_windows.h>
 
-const uint16_t PTY_SHIFT = 5;
+
 const uint8_t NUM_BLOCKS = 4;
 const int8_t BLOCK_SIZE = 16;
 const int8_t CHECK_SIZE = 10;
 const uint16_t POLY = 0x01B9;
 const uint16_t POLY_DEG = 10;
 
+const uint8_t GT_SHIFT = 12;
+const uint8_t VER_SHIFT = 11;
+const uint8_t TP_SHIFT = 10;
+const uint8_t PTY_SHIFT = 5;
+const uint8_t TA_SHIFT = 4;
+const uint8_t TEXT_AB_SHIFT = 4;
+const uint8_t MS_SHIFT = 3;
+const uint8_t DI_SHIFT = 2;
+
+const uint8_t group_0_num = 4;
+const uint8_t group_2_num = 16;
+
+
 //-----------------------------------------------------------------------------
 enum RDS_GROUP_TYPE : uint16_t
 {
 	GT_0 = 0x00,
-	GT_1 = (0x01 << 12),
-	GT_2 = (0x02 << 12),
-	GT_3 = (0x03 << 12),
-	GT_4 = (0x04 << 12),
-	GT_5 = (0x05 << 12),
-	GT_6 = (0x06 << 12),
-	GT_7 = (0x07 << 12),
-	GT_8 = (0x08 << 12),
-	GT_9 = (0x09 << 12),
-	GT_10 = (0x0A << 12),
-	GT_11 = (0x0B << 12),
-	GT_12 = (0x0C << 12),
-	GT_13 = (0x0D << 12),
-	GT_14 = (0x0E << 12),
-	GT_15 = (0x0F << 12)
+	GT_1 = 0x01,
+	GT_2 = 0x02,
+	GT_3 = 0x03,
+	GT_4 = 0x04,
+	GT_5 = 0x05,
+	GT_6 = 0x06,
+	GT_7 = 0x07,
+	GT_8 = 0x08,
+	GT_9 = 0x09,
+	GT_10 = 0x0A,
+	GT_11 = 0x0B,
+	GT_12 = 0x0C,
+	GT_13 = 0x0D,
+	GT_14 = 0x0E,
+	GT_15 = 0x0F
 };
+
+
 
 //-----------------------------------------------------------------------------
 enum RDS_VERSION : uint16_t
@@ -63,35 +79,41 @@ enum RDS_VERSION : uint16_t
 enum RDS_TP : uint16_t
 {
 	TP_0 = 0x00,			// Traffic announcements off
-	TP_1 = (0x01 << 10)		// Traffic announcements on
+	TP_1 = 0x01				// Traffic announcements on
+};
+
+//-----------------------------------------------------------------------------
+enum RDS_PTY : uint16_t
+{
+	ROCK = 5
 };
 
 //-----------------------------------------------------------------------------
 enum RDS_TA : uint16_t
 {
 	TA_0 = 0x00,			// This program carries traffic announcements but none are being broadcast at present
-	TA_1 = (0x01 << 4)		// A traffic announcement is being broadcast on this program at present
+	TA_1 = 0x01				// A traffic announcement is being broadcast on this program at present
 };
 
 //-----------------------------------------------------------------------------
 enum RDS_MS : uint16_t
 {
 	MS_0 = 0x00,			// speech
-	MS_1 = (0x01 << 3)		// music
+	MS_1 = 0x01				// music
 };
 
 //-----------------------------------------------------------------------------
 enum RDS_DI3 : uint16_t
 {
 	DI3_0 = 0x00,			// Static PTY
-	DI3_1 = (0x01 << 2)		// PTY code on the tuned service, or referenced in EON variant 13, is dynamically switched
+	DI3_1 = 0x01			// PTY code on the tuned service, or referenced in EON variant 13, is dynamically switched
 };
 
 //-----------------------------------------------------------------------------
 enum RDS_DI2 : uint16_t
 {
 	DI2_0 = 0x00,			// Not compressed
-	DI2_1 = (0x01 << 2)		// Compressed
+	DI2_1 = 0x01			// Compressed
 };
 
 //-----------------------------------------------------------------------------
@@ -121,23 +143,53 @@ inline std::vector<T> differential_encode(std::vector<T> &data, T &previous_bit)
 		previous_bit = enc_data[idx];
 	}
 
-	std::cout << std::endl << "data" << std::endl;
-	for (idx = 0; idx < data.size(); ++idx)
-	{
-		std::cout << (data[idx]) << ", ";
-	}
-	std::cout << std::endl;
+	//std::cout << std::endl << "data" << std::endl;
+	//for (idx = 0; idx < data.size(); ++idx)
+	//{
+	//	std::cout << (data[idx]) << ", ";
+	//}
+	//std::cout << std::endl;
 
-	std::cout << std::endl << "diff encode" << std::endl;
-	for (idx = 0; idx < enc_data.size(); ++idx)
-	{
-		std::cout << (enc_data[idx]) << ", ";
-	}
-	std::cout << std::endl;
+	//std::cout << std::endl << "diff encode" << std::endl;
+	//for (idx = 0; idx < enc_data.size(); ++idx)
+	//{
+	//	std::cout << (enc_data[idx]) << ", ";
+	//}
+	//std::cout << std::endl;
 
 	return enc_data;
 
 }	// end of differential_encode
+
+//-----------------------------------------------------------------------------
+template <typename T>
+std::vector<float> upsample_data(std::vector<T>& d, uint32_t factor, uint64_t sample_rate)
+{
+	uint64_t idx;
+	uint64_t index = 0;
+
+	uint64_t num_samples = d.size() * factor;
+
+	std::vector<float> u(num_samples, 0.0f);
+
+	for (idx = 0; idx < d.size(); ++idx)
+	{
+		u[index] = (float)d[idx];
+		index += factor;
+	}
+
+	//// filter the data
+	int64_t num_taps = 3*factor + 1;
+	float fc = 2400.0/(float)sample_rate;
+
+	std::vector<float> lpf = DSP::create_fir_filter<float>(num_taps, fc, &DSP::blackman_nuttall_window);
+	
+	std::vector<float> rds;
+	apply_filter(u, lpf, (num_taps>>2), rds);
+
+	return rds;
+
+}   // end of upsample_data
 
 //-----------------------------------------------------------------------------
 template <typename T>
@@ -157,12 +209,12 @@ inline std::vector<float> biphase_encode(std::vector<T>& data)
 		tmp_data_v.push_back(0);
 	}
 
-	std::cout << std::endl << "biphase 1" << std::endl;
-	for (idx = 0; idx < tmp_data_v.size(); ++idx)
-	{
-		std::cout << (tmp_data_v[idx]) << ", ";
-	}
-	std::cout << std::endl;
+	//std::cout << std::endl << "biphase 1" << std::endl;
+	//for (idx = 0; idx < tmp_data_v.size(); ++idx)
+	//{
+	//	std::cout << (tmp_data_v[idx]) << ", ";
+	//}
+	//std::cout << std::endl;
 
 	// step 2: shift by one sample and subtract
 	enc_data[0] = tmp_data_v[0];
@@ -189,7 +241,12 @@ public:
 
 	rds_block(uint16_t d_) : data(d_) {}
 
-	rds_block(uint16_t d_, uint16_t c_) : data(d_), checkword(c_) {}
+	//rds_block(uint16_t d_, uint16_t c_) : data(d_), checkword(c_) {}
+
+	rds_block(uint8_t c0, uint8_t c1)
+	{
+		data = (c0 << 8) | c1;
+	}
 
 	rds_block(const rds_block& b_) : data(b_.data), checkword(b_.checkword) {}
 
@@ -220,64 +277,94 @@ private:
 
 
 //-----------------------------------------------------------------------------
-class rds_block_1 : public rds_block
-{
-public:
-	rds_block_1(uint16_t d_)
-	{
-		data = d_;
-	}
-
-private:
-
-};
-
-//-----------------------------------------------------------------------------
-class rds_block_2 : public rds_block
-{
-public:
-
-	rds_block_2(uint16_t type, uint16_t version, uint16_t tp, uint16_t pty, uint16_t d_)
-	{
-		data = type | version | tp | pty | d_;
-	}
-
-private:
-
-};
+//class rds_block_1 : public rds_block
+//{
+//public:
+//	rds_block_1(uint16_t d_)
+//	{
+//		data = d_;
+//	}
+//
+//private:
+//
+//};
 
 //-----------------------------------------------------------------------------
-class rds_block_3 : public rds_block
-{
-public:
-
-	rds_block_3(uint16_t af1, uint16_t af2)
-	{
-		data = (af1 << 8) | af2;
-	}
-
-private:
-
-};
+//class rds_block_2 : public rds_block
+//{
+//public:
+//
+//	rds_block_2(uint16_t type, uint16_t version, uint16_t tp, uint16_t pty, uint16_t d_)
+//	{
+//		data = type | version | tp | pty | d_;
+//	}
+//
+//private:
+//
+//};
 
 //-----------------------------------------------------------------------------
-class rds_block_4 : public rds_block
+//class rds_block_3 : public rds_block
+//{
+//public:
+//
+//	rds_block_3(uint16_t af1, uint16_t af2)
+//	{
+//		data = (af1 << 8) | af2;
+//	}
+//
+//private:
+//
+//};
+
+//-----------------------------------------------------------------------------
+//class rds_block_4 : public rds_block
+//{
+//public:
+//
+//	rds_block_4(uint16_t ps)
+//	{
+//		data = ps;
+//	}
+//
+//	rds_block_4(uint8_t c0, uint8_t c1)
+//	{
+//		data = ((uint16_t)c0 << 8) | ((uint16_t)c1 & 0x00FF);
+//	}
+//
+//private:
+//
+//};
+
+typedef struct rds_params
 {
-public:
+	uint16_t pi_code;
+	uint16_t version;
+	uint16_t tp;
+	uint16_t pty;
+	uint16_t ta;
+	uint16_t ms;
 
-	rds_block_4(uint16_t ps)
-	{
-		data = ps;
-	}
+	rds_params() = default;
 
-	rds_block_4(uint8_t c0, uint8_t c1)
-	{
-		data = ((uint16_t)c0 << 8) | ((uint16_t)c1 & 0x00FF);
-	}
+	//rds_params(uint16_t pi_, uint16_t v_, uint16_t tp_, uint16_t pty_, uint16_t ta_, uint16_t ms_) : pi_code(pi_)
+	//{
+	//	version = (v_ & 0x0001) << VER_SHIFT;
+	//	tp = (tp_ & 0x0001) << TP_SHIFT;
+	//	pty = (pty_ & 0x0007) << PTY_SHIFT;
+	//	ta = (ta_ & 0x0001) << TA_SHIFT;
+	//	ms = (ms_ & 0x0001) << MS_SHIFT;
+	//}
 
-private:
+	rds_params(uint16_t pi_, uint16_t v_, uint16_t tp_, uint16_t pty_, uint16_t ta_, uint16_t ms_) : 
+		pi_code(pi_), version(v_), tp(tp_), pty(pty_), ta(ta_), ms(ms_) {}
 
-};
+	rds_params(const rds_params& rp_) : pi_code(rp_.pi_code), version(rp_.version), tp(rp_.tp),
+		pty(rp_.pty), ta(rp_.ta), ms(rp_.ms){}
+
+} rds_params;
+
+
 
 //-----------------------------------------------------------------------------
 class rds_group
@@ -300,6 +387,8 @@ public:
 
 	rds_group(std::vector<rds_block> blks)
 	{
+		blocks.clear();
+		blocks.resize(NUM_BLOCKS);
 		blocks[0] = blks[0];
 		blocks[1] = blks[1];
 		blocks[2] = blks[2];
@@ -411,6 +500,200 @@ private:
 
 };	// end of rds_group
 
+
+class rds_generator
+{
+
+public:
+	rds_params rds_param;
+
+	rds_generator(rds_params &rp) : rds_param(rp)
+	{
+		//rds_param = rds_params(rp);
+	}
+
+	//-----------------------------------------------------------------------------
+	void init_generator(std::string pn_, std::string rt_)
+	{
+		uint8_t idx;
+
+		program_name = pn_;
+		radio_text = rt_;
+
+		previous_bit = 0;
+
+		create_group_0();
+		create_group_2();
+
+		// 4 group_0 for each group_2
+		num_groups = 4 * group_2.size() + group_2.size();
+	}
+
+	//----------------------------------------------------------------------------
+	std::vector<complex<int16_t>> generate_bit_stream()
+	{
+		uint32_t idx;
+		uint32_t index = 0;
+		std::vector<int16_t> data_bits;
+
+		std::complex<float> j(0, 1);
+		const float math_2pi = 6.283185307179586476925286766559f;
+
+		group_0_index = 0;
+		group_2_index = 0;
+
+		// step 1: generate the binary bit stream
+		while(index < num_groups)
+		{
+			for (idx = 0; idx < group_0_num; ++idx)
+			{
+				std::vector<int16_t> g0_bits = group_0[idx].to_bits();
+				data_bits.insert(data_bits.end(), g0_bits.begin(), g0_bits.end());
+				++index;
+			}
+
+			std::vector<int16_t> g2_bits = group_2[group_2_index++].to_bits();
+			data_bits.insert(data_bits.end(), g2_bits.begin(), g2_bits.end());
+			++index;
+
+		}	// end of while
+
+		// step 2: apply differential encoding
+		data_bits = differential_encode(data_bits, previous_bit);
+
+		// step 3: apply biphase encoding
+		std::vector<float> data_bits_e = biphase_encode(data_bits);
+
+		// step 4: upsample and filter the data
+		std::vector<float> rds = upsample_data(data_bits_e, factor, sample_rate);
+
+		// step 5: add the pilot tone and rotate the rds data
+		float pilot_tone = 19000.0 / (float)sample_rate;
+		float rds_tone = 57000.0 / (float)sample_rate;
+
+		std::complex<float> pilot;
+		std::complex<float> rds_rot;
+
+		std::vector<complex<int16_t>> iq_data(rds.size(), std::complex<int16_t>(0, 0));
+
+		for (idx = 0; idx < rds.size(); ++idx)
+		{
+			pilot = std::complex<float>(pilot_amplitude, 0.0f) * std::exp(j * math_2pi * (pilot_tone * idx));
+			rds_rot = rds[idx] * std::exp(j * math_2pi * (rds_tone * idx));
+
+			iq_data[idx] = std::complex<int16_t>(amplitude * (pilot + rds_rot));
+		}
+
+		return iq_data;
+
+	}	// generate_bit_stream
+
+//-----------------------------------------------------------------------------
+private:
+	uint16_t group_0_index = 0;
+	uint16_t group_2_index = 0;
+	std::vector<uint16_t> di = { 0, 0, 0, 1 };			// reverse order => {d3, d2, d1, d0}
+
+	std::vector<rds_group> group_0;
+	std::vector<rds_group> group_2;
+
+	std::string program_name;
+
+	uint16_t text_ab_flag = 0;
+	std::string radio_text = "";
+
+	uint16_t num_groups = 80;
+
+	int16_t previous_bit = 0;
+	uint32_t factor = 240;
+
+	float pilot_amplitude = 0.08;
+	float rds_amplitude = 0.20;
+
+	const uint8_t samples_per_symbol = 2;
+
+	uint64_t sample_rate = (1187.5 * samples_per_symbol) * factor;
+
+	std::complex<float> amplitude = std::complex<float>(1200.0f, 0.0f);
+
+	//-----------------------------------------------------------------------------
+	void create_group_0()
+	{
+		uint8_t idx = 0;
+		uint16_t block_data = 0;
+		uint8_t pn_buffer;
+
+		std::string pgm_name_space = "        ";
+		uint8_t num_segments = 4;
+
+		if (program_name.length() < 8)
+		{
+			pn_buffer = 8 - program_name.length();
+			program_name.append(pgm_name_space, 0, pn_buffer);
+		}
+
+		group_0.clear();
+
+		rds_block b1(rds_param.pi_code);
+		rds_block b3(224, 205);				// no AF's right now
+		rds_block b2;
+		rds_block b4;
+
+		// idx is the segment address
+		for (idx = 0; idx < num_segments; ++idx)
+		{
+			block_data = (RDS_GROUP_TYPE::GT_0 << GT_SHIFT) | (rds_param.version << VER_SHIFT) | (rds_param.tp << TP_SHIFT) | (rds_param.pty << PTY_SHIFT) | (rds_param.ta << TA_SHIFT) | (rds_param.ms << MS_SHIFT) | (di[idx] << DI_SHIFT) | idx;
+			b2 = rds_block(block_data);
+			b4 = rds_block((uint8_t)(program_name[0]), (uint8_t)(program_name[1]));
+			group_0.push_back(rds_group(b1, b2, b3, b4));
+		}
+
+	}	// end of create_group_0
+
+	//-----------------------------------------------------------------------------
+	void create_group_2()
+	{
+		uint32_t idx = 0;
+		uint16_t block_data = 0;
+
+		uint8_t character_index = 0;
+
+		// determine how many characters there are
+		uint16_t num_rt_characters = radio_text.length();
+		num_rt_characters = min(num_rt_characters, 64);
+
+		if (num_rt_characters & 0x01)
+		{
+			radio_text.append(" ", 0, 1);
+		}
+
+		uint8_t num_segments = radio_text.length() >> 2;
+
+		group_2.clear();
+
+		rds_block b1(rds_param.pi_code);
+		rds_block b2;
+		rds_block b3;
+		rds_block b4;
+
+		text_ab_flag ^= 1;
+		
+		// idx is the segment address
+		for (idx = 0; idx < num_segments; ++idx)
+		{
+			block_data = (RDS_GROUP_TYPE::GT_2 << GT_SHIFT) | (rds_param.version << VER_SHIFT) | (rds_param.tp << TP_SHIFT) | (rds_param.pty << PTY_SHIFT) | (text_ab_flag << TEXT_AB_SHIFT) | idx;
+			b2 = rds_block(block_data);
+			b3 = rds_block((uint8_t)(radio_text[character_index++]), (uint8_t)(radio_text[character_index++]));
+			b4 = rds_block((uint8_t)(radio_text[character_index++]), (uint8_t)(radio_text[character_index++]));
+			group_2.push_back(rds_group(b1, b2, b3, b4));
+		}
+
+	}	// end of create_group_2
+
+	//-----------------------------------------------------------------------------
+
+
+};	// end or rds_generator
 
 
 #endif	// RDS_HEADER_H_
