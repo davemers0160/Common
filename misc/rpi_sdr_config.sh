2cfde1e7680867de35494c921152747fdacbff0c
david emerson <davemers0160@gmail.com>
2024-08-26 19:45:23 -0400

    Update rpi_sdr_config.sh

--- misc/rpi_sdr_config.sh
+++ misc/rpi_sdr_config.sh
@@ -1,5 +1,23 @@
 #!/bin/bash
 
+# -----------------------------------------------------------------------------
+# Instructions for this script
+# From windows you can sftp into the pi and copy this repo into /home/{$USER}/Projects
+# sftp loki@xxx.xxx.xxx.xxx
+# 
+# make the project folder if it doesn't already exist
+# mkdir Projects
+#
+# put this file in the user home directory and then run the following:
+# chmod +x rpi_sdr_config.sh
+# ./rpi_sdr_config.sh
+# -----------------------------------------------------------------------------
+
+# -----------------------------------------------------------------------------
+# if the script does not run initially you may need to run the following command and then retry the script
+# dos2unix rpi_sdr_config.sh
+# -----------------------------------------------------------------------------
+
 sudo apt-get update
 sudo apt-get install -y build-essential git cmake libusb-1* libsndfile1
 
@@ -19,8 +37,12 @@ source ~/venv/bin/activate
 pip install numpy pyyaml soundfile
 
 # grab all of the projects 
-mkdir Projects
+mkdir -p Projects
 cd Projects
+
+echo " "
+echo "Building BladeRF project"
+echo " "
 
 git clone https://github.com/Nuand/bladeRF.git ./bladeRF
 cd ./bladeRF/host
@@ -31,13 +53,18 @@ make -j 4 && sudo make install && sudo ldconfig
 
 cd ~/Projects
 
-mkdir data
+# make the data directory
+mkdir -p data
 
 # grab the common, python_common and the rapipyaml repos
 git clone https://github.com/davemers0160/Common
 git clone https://github.com/davemers0160/python_common
 
 git clone --recursive https://github.com/davemers0160/rapidyaml
+
+echo " "
+echo "Building rapidyaml project"
+echo " "
 
 # build the rapidyaml library
 cd rapidyaml
@@ -53,7 +80,7 @@ cd ~/Projects
 git clone https://github.com/davemers0160/SDR
 
 # build the tx hop example
-cd SDR/bladerf/tx_hop_example
+cd SDR/bladerf/tx_example
 mkdir build
 cd build
 cmake ..
@@ -66,9 +93,14 @@ sudo cp /home/${USER}/Projects/SDR/bladerf/common/bladerf.service /lib/systemd/s
 # reload the systemd daemon
 sudo systemctl daemon-reload
 
-# enable the bladerf serrvice and autostart
-sudo systemctl enable bladerf.service
+# enable the bladerf service and autostart
+# sudo systemctl enable bladerf.service
 
+echo " "
+echo "Run the following command to enable the bladerf automatic start service"
+echo "sudo systemctl enable bladerf.service"
+echo " "
 echo "Run the following command:"
 echo "sudo nano /boot/firmware/cmdline.txt"
 echo "- add the following to the end of the line: \" usbcore.usbfs_memory_mb=2048 \" "
+
