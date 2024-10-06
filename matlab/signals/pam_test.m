39e5d3c54d8422bea4602cc940926d7f196f1a1d
david emerson <davemers0160@gmail.com>
2024-10-06 14:15:34 -0400

    Update pam_test.m

--- matlab/signals/pam_test.m
+++ matlab/signals/pam_test.m
@@ -12,11 +12,11 @@ plot_num = 1;
 
 %% parameters
 
-beta = 0.6;
-span = 7;
-sample_rate = 50e6;
+beta = 0.9;
+span = 9;
+sample_rate = 1e6;
 symbol_length = 0.001;
-scale = 1;
+scale = 20;
 amplitude = 2000;
 
 samples_per_symbol = floor(sample_rate*symbol_length + 0.5);
@@ -74,11 +74,28 @@ for idx = 1:num_data2
     d2(index-N*samples_per_symbol+1:index+N*samples_per_symbol+1) = d2(index-N*samples_per_symbol+1:index+N*samples_per_symbol+1) + rrc_trunc * data2(idx);
     index = index + samples_per_symbol;
 end
+
+% d3 = conv(d2, (1/9)*[1,1,1,1,1,1,1,1,1], 'same');
 toc
 
+%% fft test
+rrc_fft = fft(g, numel(iq_data)).';
+
+tic
+iq_fft = fft(iq_data);
+
+tmp = iq_fft .* rrc_fft;
+
+d3 = ifft(tmp);
+
+toc
+
+
+%%
 figure(plot_num);
-plot(d2*80,'b'); 
+plot(d2,'b'); 
 hold on; 
-plot(cat(1,zeros(N*samples_per_symbol,1),iq2*80),'g'); 
+plot(cat(1,zeros(N*samples_per_symbol,1),iq2),'g'); 
 plot(cat(1,zeros(N*samples_per_symbol,1),iq_data),'r');
+plot((d3),'m'); 
 plot_num = plot_num + 1;
