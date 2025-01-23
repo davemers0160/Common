b21236884c09f4eee3931113dd0674dc933cad9b
david emerson <davemers0160@gmail.com>
2025-01-23 00:36:23 -0500

    Update iq_analysis.m

--- matlab/iq_utils/iq_analysis.m
+++ matlab/iq_utils/iq_analysis.m
@@ -48,10 +48,9 @@ if(isempty(res))
 end
 
 fs = str2double(res{1});
-
-%%
 t = 0:1/fs:(numel(iqc_in)-1)/fs;
 
+%%
 % iqc = iqc()
 max(real(iqc_in))
 min(real(iqc_in))
@@ -60,6 +59,7 @@ max(imag(iqc_in))
 min(imag(iqc_in))
 
 iqc = scale * iqc_in;
+tmt = timetable(seconds(t.'), iqc);
 
 %%
 iq(:,1) = real(iqc);
@@ -126,14 +126,15 @@ plot_num = plot_num + 1;
 
 %% filtered
 
-num_taps = 31;
+step = 1;
+num_taps = 15;
 w = hamming(num_taps);
-fc = 2.5e6/fs;
+fc = 0.4e6/fs;
 lpf = create_fir_filter(fc, w);
 
 iqc_f = conv(iqc, lpf(end:-1:1),'same');
 
-[s, f, ts] = spectrogram(iqc_f, 512, 480, 512, fs, 'centered');
+[s, f, ts] = spectrogram(iqc_f(1:step:end), 512, 480, 512, fs/step, 'centered');
 
 figure(plot_num)
 set(gcf,'position',([50,50,1000,800]),'color','w')
@@ -151,6 +152,42 @@ ylabel('Frequency (MHz)', 'fontweight','bold','FontSize',12);
 view(90,-90);
 plot_num = plot_num + 1;
 drawnow;
+
+
+figure(plot_num)
+plot(t(1:step:end), real(iqc_f(1:step:end)),'b')
+hold on
+plot(t(1:step:end), imag(iqc_f(1:step:end)),'r')
+plot_num = plot_num + 1;
+drawnow;
+
+%%
+const_diag = comm.ConstellationDiagram;
+
+step = 50;
+const_diag(iqc(1:step:end));
+
+
+%% eye diagram
+
+prompt = {'samples per symbol:'};
+dlgtitle = 'Input';
+fieldsize = [1 30];
+definput = {'6'};
+
+res = inputdlg(prompt, dlgtitle, fieldsize, definput);
+
+if(isempty(res))
+    return;
+end
+
+samples_per_symbol = str2double(res{1});
+
+step = 25;
+eyediagram(iqc(1:step:5000), samples_per_symbol)
+
+plot_num = plot_num + 1;
+
 
 %%
 % figure(plot_num)
@@ -210,15 +247,16 @@ drawnow;
 
 %%
 
-iq_start = 1;
-iq_stop = numel(iqc);
+iq_start = ceil(fs*1.969e-5);
+iq_stop = floor(fs*0.0028); %numel(iqc);
+step = 50;
 
 figure(plot_num)
 set(gcf,'position',([50,50,1400,500]),'color','w')
 
-scatter3(t(iq_start:iq_stop), real(iqc(iq_start:iq_stop)), imag(iqc(iq_start:iq_stop)), 20, 'o', 'b', 'filled');
+scatter3(t(iq_start:step:iq_stop), real(iqc(iq_start:step:iq_stop)), imag(iqc(iq_start:step:iq_stop)), 20, 'o', 'b', 'filled');
 hold on
-plot3(t(iq_start:iq_stop), real(iqc(iq_start:iq_stop)), imag(iqc(iq_start:iq_stop)), 'b');
+% plot3(t(iq_start:step:iq_stop), real(iqc(iq_start:step:iq_stop)), imag(iqc(iq_start:step:iq_stop)), 'b');
 
 set(gca,'fontweight','bold','FontSize',11);
 
@@ -231,6 +269,63 @@ drawnow;
 
 %%
 return;
+
+%% comet plot
+
+figure(plot_num)
+set(gcf,'position',([50,50,1400,500]),'color','w')
+hold on
+set(gca,'fontweight','bold','FontSize',11);
+
+xlabel('time (s)', 'fontweight','bold');
+ylabel('I', 'fontweight','bold');
+zlabel('Q', 'fontweight','bold');
+
+view(-30,45);
+
+for idx=1:numel(iqc)
+    scatter3(t(idx), real(iqc(idx)), imag(iqc(idx)), 20, 'o', 'b', 'filled');
+    % plot3(t(iq_start:step:iq_stop), real(iqc(iq_start:step:iq_stop)), imag(iqc(iq_start:step:iq_stop)), 'b');
+    % pause(0.0001);
+    drawnow;
+end
+
+
+plot_num = plot_num + 1;
+drawnow;
+
+%% comet3
+
+figure;
+set(gcf,'position',([50,50,1400,500]),'color','w')
+
+set(gca,'fontweight','bold','FontSize',11);
+grid on
+box on
+
+xlabel('time (s)', 'fontweight','bold');
+ylabel('I', 'fontweight','bold');
+zlabel('Q', 'fontweight','bold');
+
+view(-30,45);
+
+f1 = gcf;
+tailFormat = struct('LineWidth',1,'Color','b','LineStyle','-');
+headFormat = struct('LineStyle','none','Marker','o','MarkerSize',6,'Color','r', 'MarkerFaceColor', 'b');
+PlotComet_3D(t, real(iqc), imag(iqc), 'cFigure', f1, 'Frequency', 1000000, 'blockSize', 1000, 'tailFormat',tailFormat, 'headFormat',headFormat);
+% PlotComet_3D(real(iqc), imag(iqc), t, 'Frequency', 100000, 'blockSize', 1000, 'tailFormat',tailFormat, 'headFormat',headFormat);
+
+plot_num = plot_num + 1;
+
+%% FM demod
+
+fmdemod = comm.FMDemodulator("SampleRate",fs, "FrequencyDeviation", 100e3)
+
+z = fmdemod(iqc);
+mz = max(abs(z));
+ap=audioplayer(z/mz, 44100);
+play(ap);
+
 %%
 % figure;
 % h = animatedline;
