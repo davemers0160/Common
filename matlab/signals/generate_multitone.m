58243aa57dd220d414d20cf3c5f48630e2f3ad8a
david emerson <davemers0160@gmail.com>
2026-06-21 10:43:03 -0400

    Update generate_multitone.m

--- matlab/signals/generate_multitone.m
+++ matlab/signals/generate_multitone.m
@@ -4,14 +4,13 @@ function iq_data = generate_multitone(sample_rate, duration, amplitude, freq_sta
     num_samples = round(sample_rate * duration);
 
     % generate the tone frequencies
-    tones = freq_start:tone_spacing:freq_stop;
+    tones = (freq_start:tone_spacing:freq_stop).';
     num_tones = length(tones);
 
     % time step
     t = (0:(num_samples-1))/sample_rate;
 
     iq_data = zeros(1,num_samples);
-
     for idx=1:num_tones
         iq_data = iq_data + (amplitude/num_tones) * exp(1i*2.0*pi()* tones(idx) * t);
     end 
